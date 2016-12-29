306f628e049eefc76ccb457d1e72e9a892840b1b
pavlobaron <pb@pbit.org>
2016-12-29 19:45:29 +0100

    delta teh delta metrics

--- instana/meter.py
+++ instana/meter.py
@@ -53,6 +53,7 @@ class Meter(object):
     SNAPSHOT_PERIOD = 600
     snapshot_countdown = 1
     sensor = None
+    last_usage = None
 
     def __init__(self, sensor):
         self.sensor = sensor
@@ -94,21 +95,23 @@ class Meter(object):
 
     def collect_metrics(self):
         u = resource.getrusage(resource.RUSAGE_SELF)
-        m = Metrics(ru_utime=u[0],
-                    ru_stime=u[1],
+        m = Metrics(ru_utime=u[0] if not self.last_usage else u[0] - self.last_usage[0],
+                    ru_stime=u[1] if not self.last_usage else u[1] - self.last_usage[1],
                     ru_maxrss=u[2],
                     ru_ixrss=u[3],
                     ru_idrss=u[4],
                     ru_isrss=u[5],
-                    ru_minflt=u[6],
-                    ru_majflt=u[7],
-                    ru_nswap=u[8],
-                    ru_inblock=u[9],
-                    ru_oublock=u[10],
-                    ru_msgsnd=u[11],
-                    ru_msgrcv=u[12],
-                    ru_nsignals=u[13],
-                    ru_nvcs=u[14],
-                    ru_nivcsw=u[15])
+                    ru_minflt=u[6] if not self.last_usage else u[6] - self.last_usage[6],
+                    ru_majflt=u[7] if not self.last_usage else u[7] - self.last_usage[7],
+                    ru_nswap=u[8] if not self.last_usage else u[8] - self.last_usage[8],
+                    ru_inblock=u[9] if not self.last_usage else u[9] - self.last_usage[9],
+                    ru_oublock=u[10] if not self.last_usage else u[10] - self.last_usage[10],
+                    ru_msgsnd=u[11] if not self.last_usage else u[11] - self.last_usage[11],
+                    ru_msgrcv=u[12] if not self.last_usage else u[12] - self.last_usage[12],
+                    ru_nsignals=u[13] if not self.last_usage else u[13] - self.last_usage[13],
+                    ru_nvcs=u[14] if not self.last_usage else u[14] - self.last_usage[14],
+                    ru_nivcsw=u[15] if not self.last_usage else u[15] - self.last_usage[15])
+
+        self.last_usage = u
 
         return m

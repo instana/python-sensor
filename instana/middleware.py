1def0a1e8de2192edc50d2de5430cd310feedb5f
Peter Giacomo Lombardo <pglombardo@gmail.com>
2017-07-06 18:50:50 +0200

    Pickup and return context from django middleware

--- instana/middleware.py
+++ instana/middleware.py
@@ -1,4 +1,4 @@
-import opentracing
+import opentracing as ot
 import instana.tracer
 import opentracing.ext.tags as ext
 
@@ -6,19 +6,25 @@ import opentracing.ext.tags as ext
 class InstanaMiddleware(object):
     def __init__(self, get_response):
         self.get_response = get_response
-        opentracing.global_tracer = instana.tracer.InstanaTracer()
+        ot.global_tracer = instana.tracer.InstanaTracer()
         self
 
     def __call__(self, request):
-        span = opentracing.global_tracer.start_span("django")
+        env = request.environ
+        if 'HTTP_X_INSTANA_T' in env and 'HTTP_X_INSTANA_S' in env:
+            ctx = ot.global_tracer.extract(ot.Format.HTTP_HEADERS, env)
+            span = ot.global_tracer.start_span("django", child_of=ctx)
+        else:
+            span = ot.global_tracer.start_span("django")
 
-        span.set_tag(ext.HTTP_URL, request.environ['PATH_INFO'])
-        span.set_tag("http.params", request.environ['QUERY_STRING'])
+        span.set_tag(ext.HTTP_URL, env['PATH_INFO'])
+        span.set_tag("http.params", env['QUERY_STRING'])
         span.set_tag(ext.HTTP_METHOD, request.method)
-        span.set_tag("http.host", request.environ['HTTP_HOST'])
+        span.set_tag("http.host", env['HTTP_HOST'])
 
         response = self.get_response(request)
 
         span.set_tag(ext.HTTP_STATUS_CODE, response.status_code)
+        ot.global_tracer.inject(span.context, ot.Format.HTTP_HEADERS, response)
         span.finish()
         return response

5dfe8632d2cc2c2560f961ceeaf7838607bf62b7
Peter Giacomo Lombardo <pglombardo@gmail.com>
2017-07-25 11:52:37 +0200

    Protect inject/extract with try/catch
    ...to avoid breaking customer apps in case of failure.

--- instana/propagator.py
+++ instana/propagator.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 import opentracing as ot
-from instana import util
+from instana import util, log
 
 prefix_tracer_state = 'HTTP_X_INSTANA_'
 field_name_trace_id = prefix_tracer_state + 'T'
@@ -12,27 +12,33 @@ class HTTPPropagator():
     """A Propagator for Format.HTTP_HEADERS. """
 
     def inject(self, span_context, carrier):
-        carrier[field_name_trace_id] = util.id_to_header(span_context.trace_id)
-        carrier[field_name_span_id] = util.id_to_header(span_context.span_id)
+        try:
+            carrier[field_name_trace_id] = util.id_to_header(span_context.trace_id)
+            carrier[field_name_span_id] = util.id_to_header(span_context.span_id)
+        except Exception as e:
+            log.debug("inject error: ", str(e))
 
     def extract(self, carrier):  # noqa
-        count = 0
-        span_id, trace_id = (0, 0)
-        for k in carrier:
-            v = carrier[k]
-            k = k.lower()
-            if k == field_name_span_id:
-                span_id = util.header_to_id(v)
-                count += 1
-            elif k == field_name_trace_id:
-                trace_id = util.header_to_id(v)
-                count += 1
+        try:
+            count = 0
+            span_id, trace_id = (0, 0)
+            for k in carrier:
+                v = carrier[k]
+                k = k.lower()
+                if k == field_name_span_id:
+                    span_id = util.header_to_id(v)
+                    count += 1
+                elif k == field_name_trace_id:
+                    trace_id = util.header_to_id(v)
+                    count += 1
 
-        if count != field_count:
-            raise ot.SpanContextCorruptedException()
+            if count != field_count:
+                raise ot.SpanContextCorruptedException()
 
-        return ot.SpanContext(
-            span_id=span_id,
-            trace_id=trace_id,
-            baggage={},
-            sampled=True)
+            return ot.SpanContext(
+                span_id=span_id,
+                trace_id=trace_id,
+                baggage={},
+                sampled=True)
+        except Exception as e:
+            log.debug("inject error: ", str(e))

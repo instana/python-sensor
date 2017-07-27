71ea5ba8886c4a355499933e59dfb2f32e976b89
Peter Giacomo Lombardo <pglombardo@gmail.com>
2017-07-27 13:41:27 +0200

    Add runtime only hook & instrumentation

---
+++ instana/runtime.py
@@ -0,0 +1,14 @@
+import opentracing as ot
+from instana import tracer, options
+import logging
+import os
+
+
+def hook(module):
+    """ Hook method to install the Instana middleware into Flask """
+    if "INSTANA_DEV" in os.environ:
+        level = logging.DEBUG
+    else:
+        level = logging.WARN
+    opts = options.Options(log_level=level)
+    ot.global_tracer = tracer.InstanaTracer(opts)

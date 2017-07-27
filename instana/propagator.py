0e50715568166abde62b1ff7983dae35e7a24810
Peter Giacomo Lombardo <pglombardo@gmail.com>
2017-07-27 15:25:49 +0200

    Simplify and add list/tuple support for extract

--- instana/propagator.py
+++ instana/propagator.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 import opentracing as ot
+from basictracer.context import SpanContext
 from instana import util, log
 
 prefix_tracer_state = 'HTTP_X_INSTANA_'
@@ -31,25 +32,21 @@ class HTTPPropagator():
 
     def extract(self, carrier):  # noqa
         try:
-            count = 0
-            span_id, trace_id = (0, 0)
-            for k in carrier:
-                v = carrier[k]
-                k = k.lower()
-                if k == field_name_span_id:
-                    span_id = util.header_to_id(v)
-                    count += 1
-                elif k == field_name_trace_id:
-                    trace_id = util.header_to_id(v)
-                    count += 1
-
-            if count != field_count:
+            if type(carrier) is dict:
+                dc = carrier
+            elif type(carrier) is list:
+                dc = dict(carrier)
+            else:
                 raise ot.SpanContextCorruptedException()
 
-            return ot.SpanContext(
-                span_id=span_id,
-                trace_id=trace_id,
-                baggage={},
-                sampled=True)
+            if field_name_trace_id in dc and field_name_span_id in dc:
+                trace_id = util.header_to_id(dc[field_name_trace_id])
+                span_id = util.header_to_id(dc[field_name_span_id])
+
+            return SpanContext(span_id=span_id,
+                               trace_id=trace_id,
+                               baggage={},
+                               sampled=True)
+
         except Exception as e:
             log.debug("extract error: ", str(e))

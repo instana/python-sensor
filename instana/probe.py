fc35b177558b64dcf265ef82cd88253f39c99a54
Peter Giacomo Lombardo <pglombardo@gmail.com>
2017-12-09 19:41:30 +0100

    Hook & probe initializers.

---
+++ instana/probe.py
@@ -0,0 +1,13 @@
+import opentracing as ot
+from instana import tracer, options
+
+# This file is the hook for autoinstrumenation.
+# Here, we should:
+#  1. Make sure instana sensor is not already active in the process
+#  2. Activate properly
+#    a. Runtime metrics
+#    b. Detect and instrument framework
+#    c. Detect and instrument any libraries
+
+opts = options.Options()
+ot.global_tracer = tracer.InstanaTracer(opts)

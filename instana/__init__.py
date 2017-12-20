1805cce734849c54f05d4fa1c8005c6261ba4783
Peter Giacomo Lombardo <pglombardo@gmail.com>
2017-12-20 12:48:23 +0100

    Bump package version to 0.6.12

--- instana/__init__.py
+++ instana/__init__.py
@@ -9,7 +9,7 @@ __author__ = 'Instana Inc.'
 __copyright__ = 'Copyright 2017 Instana Inc.'
 __credits__ = ['Pavlo Baron', 'Peter Giacomo Lombardo']
 __license__ = 'MIT'
-__version__ = '0.6.11'
+__version__ = '0.6.12'
 __maintainer__ = 'Peter Giacomo Lombardo'
 __email__ = 'peter.lombardo@instana.com'
 

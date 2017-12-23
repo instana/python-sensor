697d56dcd4aae19e6cb2351eb39a5c195f8ed029
Peter Giacomo Lombardo <pglombardo@gmail.com>
2017-12-23 09:56:41 +0100

    Expand 5xx coverage; log exceptions

--- instana/instrumentation/urllib3.py
+++ instana/instrumentation/urllib3.py
@@ -1,4 +1,6 @@
+from __future__ import absolute_import
 import opentracing.ext.tags as ext
+import instana
 import opentracing
 import wrapt
 
@@ -6,21 +8,21 @@ import wrapt
 @wrapt.patch_function_wrapper('urllib3', 'PoolManager.urlopen')
 def urlopen_with_instana(wrapped, instance, args, kwargs):
     try:
-        span = opentracing.global_tracer.start_span("urllib3")
+        span = instana.internal_tracer.start_span("urllib3")
         span.set_tag(ext.HTTP_URL, args[1])
         span.set_tag(ext.HTTP_METHOD, args[0])
 
-        opentracing.global_tracer.inject(span.context, opentracing.Format.HTTP_HEADERS, kwargs["headers"])
-
+        instana.internal_tracer.inject(span.context, opentracing.Format.HTTP_HEADERS, kwargs["headers"])
         rv = wrapped(*args, **kwargs)
+
         span.set_tag(ext.HTTP_STATUS_CODE, rv.status)
-        if 500 <= rv.status <= 511:
+        if 500 <= rv.status <= 599:
             span.set_tag("error", True)
             ec = span.tags.get('ec', 0)
             span.set_tag("ec", ec+1)
 
     except Exception as e:
-        print("found error:", e)
+        span.log_kv({'message': e})
         span.set_tag("error", True)
         ec = span.tags.get('ec', 0)
         span.set_tag("ec", ec+1)

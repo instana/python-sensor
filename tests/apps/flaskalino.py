e86f88b0ab58ecb7951dad2964f8fa2ec6647710
Peter Giacomo Lombardo <pglombardo@gmail.com>
2017-12-23 09:57:03 +0100

    More routes to test against

--- tests/apps/flaskalino.py
+++ tests/apps/flaskalino.py
@@ -11,5 +11,30 @@ def hello():
     return "<center><h1>🐍 Hello Stan! 🦄</h1></center>"
 
 
+@app.route("/400")
+def fourhundred():
+    return "Bad Request", 400
+
+
+@app.route("/405")
+def fourhundredfive():
+    return "Method not allowed", 405
+
+
+@app.route("/500")
+def fivehundred():
+    return "Internal Server Error", 500
+
+
+@app.route("/504")
+def fivehundredfour():
+    return "Gateway Timeout", 504
+
+
+@app.route("/exception")
+def exception():
+    raise Exception('fake error')
+
+
 if __name__ == '__main__':
     app.run()

ad914ab1f8bff9c86f9fba57fd490a5920df1094
Peter Giacomo Lombardo <pglombardo@gmail.com>
2017-12-27 14:46:36 +0100

    Have instrumentation always use internal_tracer

--- instana/django19.py
+++ instana/django19.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 import opentracing as ot
-from instana import tracer, options
+from instana import internal_tracer
 import opentracing.ext.tags as ext
 import os
 
@@ -11,18 +11,16 @@ DJ19_INSTANA_MIDDLEWARE = 'instana.django19.InstanaMiddleware19'
 class InstanaMiddleware19(object):
     """ Django 1.9 Middleware to provide request tracing for Instana """
     def __init__(self):
-        opts = options.Options(service="Django")
-        ot.tracer = tracer.InstanaTracer(opts)
         self.span = None
         self
 
     def process_request(self, request):
         env = request.environ
         if 'HTTP_X_INSTANA_T' in env and 'HTTP_X_INSTANA_S' in env:
-            ctx = ot.tracer.extract(ot.Format.HTTP_HEADERS, env)
-            span = ot.tracer.start_span("django", child_of=ctx)
+            ctx = internal_tracer.extract(ot.Format.HTTP_HEADERS, env)
+            span = internal_tracer.start_span("django", child_of=ctx)
         else:
-            span = ot.tracer.start_span("django")
+            span = internal_tracer.start_span("django")
 
         span.set_tag(ext.HTTP_URL, env['PATH_INFO'])
         span.set_tag("http.params", env['QUERY_STRING'])
@@ -38,7 +36,7 @@ class InstanaMiddleware19(object):
                 self.span.set_tag("ec", ec+1)
 
             self.span.set_tag(ext.HTTP_STATUS_CODE, response.status_code)
-            ot.tracer.inject(self.span.context, ot.Format.HTTP_HEADERS, response)
+            internal_tracer.inject(self.span.context, ot.Format.HTTP_HEADERS, response)
             self.span.finish()
             self.span = None
         return response

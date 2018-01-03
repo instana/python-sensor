378b1bc23a5ed5321ea0575e58477d57c675d908
Peter Giacomo Lombardo <pglombardo@gmail.com>
2018-01-04 00:26:24 +0100

    Better imports & remove service_name from sensor

--- instana/sensor.py
+++ instana/sensor.py
@@ -1,33 +1,28 @@
-import instana.options as o
-import instana.meter as m
-import instana.agent as a
-from instana import log
+from __future__ import absolute_import
+from .options import Options
+from .meter import Meter
+from .agent import Agent
+import instana.log as log
 
 
 class Sensor(object):
     options = None
     meter = None
-    service_name = None
     agent = None
 
     def __init__(self, options):
         self.set_options(options)
         log.init(options.log_level)
-        self.configure_service_name()
-        self.agent = a.Agent(self)
-        self.meter = m.Meter(self)
+        self.agent = Agent(self)
+        self.meter = Meter(self)
 
         log.debug("initialized sensor")
 
     def set_options(self, options):
         self.options = options
         if not self.options:
-            self.options = o.Options()
-
-    def configure_service_name(self):
-        if self.options:
-            self.service_name = self.options.service
+            self.options = Options()
 
     def handle_fork(self):
-        self.agent = a.Agent(self)
-        self.meter = m.Meter(self)
+        self.agent = Agent(self)
+        self.meter = Meter(self)

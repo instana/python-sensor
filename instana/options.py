6474bc634638319249146c30713ec600d7d0aa6a
Peter Giacomo Lombardo <pglombardo@gmail.com>
2018-01-04 00:22:59 +0100

    Have Options respect any standard env vars that are set.

--- instana/options.py
+++ instana/options.py
@@ -9,7 +9,16 @@ class Options(object):
     log_level = logging.WARN
 
     def __init__(self, **kwds):
+        """ Initialize Options
+        Respect any environment variables that may be set.
+        """
         if "INSTANA_DEV" in os.environ:
             self.log_level = logging.DEBUG
+
+        if "INSTANA_AGENT_IP" in os.environ:
+            self.agent_host = os.environ["INSTANA_AGENT_IP"]
+
+        if "INSTANA_AGENT_PORT" in os.environ:
+            self.agent_port = os.environ["INSTANA_AGENT_PORT"]
 
         self.__dict__.update(kwds)

a98e106092a8eceda27f16962aebce08c8507d1a
Peter Giacomo Lombardo <pglombardo@gmail.com>
2018-02-04 11:08:39 +0100

    Bump package version to 0.7.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='instana',
-      version='0.7.6',
+      version='0.7.7',
       download_url='https://github.com/instana/python-sensor',
       url='https://www.instana.com/',
       license='MIT',

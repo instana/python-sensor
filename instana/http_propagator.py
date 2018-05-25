d10d05ef3a1751b9db4f0bffdc299167f08fabe5
Marco Garcia <marco.garcia@mgarcia-m.fios-router.home>
2018-05-25 13:52:06 -0400

    Adding GRPC safe HTTP headers

--- instana/http_propagator.py
+++ instana/http_propagator.py
@@ -26,6 +26,19 @@ class HTTPPropagator():
     ALT_HEADER_KEY_T = 'HTTP_X_INSTANA_T'
     ALT_HEADER_KEY_S = 'HTTP_X_INSTANA_S'
     ALT_HEADER_KEY_L = 'HTTP_X_INSTANA_L'
+    GRPC_SAFE_HEADER_KEY_T = 'x-instana-t'
+    GRPC_SAFE_HEADER_KEY_S = 'x-instana-s'
+    GRPC_SAFE_HEADER_KEY_L = 'x-instana-l'
+    
+    def __init__(self, use_grpc_safe_headers):
+        if(use_grpc_safe_headers):
+            self.header_key_t = self.GRPC_SAFE_HEADER_KEY_T
+            self.header_key_s = self.GRPC_SAFE_HEADER_KEY_S
+            self.header_key_l = self.GRPC_SAFE_HEADER_KEY_L
+        else:
+            self.header_key_t = self.HEADER_KEY_T
+            self.header_key_s = self.HEADER_KEY_S
+            self.header_key_l = self.HEADER_KEY_L
 
     def inject(self, span_context, carrier):
         try:
@@ -33,13 +46,13 @@ class HTTPPropagator():
             span_id = util.id_to_header(span_context.span_id)
 
             if type(carrier) is dict or hasattr(carrier, "__dict__"):
-                carrier[self.HEADER_KEY_T] = trace_id
-                carrier[self.HEADER_KEY_S] = span_id
-                carrier[self.HEADER_KEY_L] = "1"
+                carrier[self.header_key_t] = trace_id
+                carrier[self.header_key_s] = span_id
+                carrier[self.header_key_l] = "1"
             elif type(carrier) is list:
-                carrier.append((self.HEADER_KEY_T, trace_id))
-                carrier.append((self.HEADER_KEY_S, span_id))
-                carrier.append((self.HEADER_KEY_L, "1"))
+                carrier.append((self.header_key_t, trace_id))
+                carrier.append((self.header_key_s, span_id))
+                carrier.append((self.header_key_l, "1"))
             else:
                 raise Exception("Unsupported carrier type", type(carrier))
 
@@ -60,10 +73,15 @@ class HTTPPropagator():
                 trace_id = util.header_to_id(dc[self.HEADER_KEY_T])
                 span_id = util.header_to_id(dc[self.HEADER_KEY_S])
 
-            # Alternatively check for alternate HTTP_X_INSTANA_T/S style
+            # Check for alternate HTTP_X_INSTANA_T/S style
             elif self.ALT_HEADER_KEY_T in dc and self.ALT_HEADER_KEY_S in dc:
                 trace_id = util.header_to_id(dc[self.ALT_HEADER_KEY_T])
                 span_id = util.header_to_id(dc[self.ALT_HEADER_KEY_S])
+
+            # Check for GRPC safe x-instana-t/s style
+            elif self.GRPC_SAFE_HEADER_KEY_T in dc and self.GRPC_SAFE_HEADER_KEY_S in dc:
+                trace_id = util.header_to_id(dc[self.GRPC_SAFE_HEADER_KEY_T])
+                span_id = util.header_to_id(dc[self.GRPC_SAFE_HEADER_KEY_S])
 
             return SpanContext(span_id=span_id,
                                trace_id=trace_id,

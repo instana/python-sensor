9424b6a21226c33d626114d99b27724e26f7f8ed
Peter Giacomo Lombardo <pglombardo@gmail.com>
2018-07-02 19:41:33 +0200

    MySQLdb Instrumentation (mysql-python) (#84)
    * Fix background server names.
    
    * More attempts to quiet spyne background server
    
    * Initial MySQLdb instrumentation and basic tests.
    
    * Add Mysql-python to test bundle; fix python 3 test runs
    
    * Fix Python version test
    
    * Update import to use new locations
    
    * Make sure pip is updated before package installation
    
    * Instrumentation safeties; executemany impl
    
    * Centralize KV collection; callproc support
    
    * Upgrade setuptools for python 3.3 issue
    
    * For Travis, use a blank password
    
    * Create mysql db before test run
    
    * formatting
    
    * formatting
    
    * Trigger Travis build
    
    * Trigger Travis build
    
    * Trigger Travis build
    
    * Make sure message has a message
    
    * Add test to validate error capture/logging

--- setup.py
+++ setup.py
@@ -26,10 +26,11 @@ setup(name='instana',
         'test': [
             'nose>=1.0',
             'flask>=0.12.2',
+            'lxml>=3.4',
+            'MySQL-python>=1.2.5;python_version<="2.7"',
             'requests>=2.17.1',
             'urllib3[secure]>=1.15',
             'spyne>=2.9',
-            'lxml>=3.4',
             'suds-jurko>=0.6'
         ],
       },

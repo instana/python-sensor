b049f77571690a62c2343d56f304469e8838677d
Peter Giacomo Lombardo <pglombardo@gmail.com>
2018-11-22 18:06:31 +0100

    Bump package version to 1.8.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def check_setuptools():
 check_setuptools()
 
 setup(name='instana',
-      version='1.8.0',
+      version='1.8.1',
       download_url='https://github.com/instana/python-sensor',
       url='https://www.instana.com/',
       license='MIT',

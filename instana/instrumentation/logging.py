8dff0bada6c046fd8a39934c8f3203c3abb2832d
steveww <s@steveww.org>
2019-02-07 16:02:48 +0000

    error handling

--- instana/instrumentation/logging.py
+++ instana/instrumentation/logging.py
@@ -12,32 +12,34 @@ def log_with_instana(wrapped, instance, argv, kwargs):
     # argv[0] = level
     # argv[1] = message
     # argv[2] = args for message
-    parent_span = tracer.active_span
+    try:
+        parent_span = tracer.active_span
 
-    # Only needed if we're tracing and serious log
-    if parent_span and argv[0] >= logging.WARN:
-        # get the formatted log message
-        msg = argv[1] % argv[2]
+        # Only needed if we're tracing and serious log
+        if parent_span and argv[0] >= logging.WARN:
+            # get the formatted log message
+            msg = argv[1] % argv[2]
 
-        # get additional information if an exception is being handled
-        parameters = None
-        (t, v, tb) = sys.exc_info()
-        if t is not None and v is not None:
-            parameters = '{} {}'.format(t , v)
+            # get additional information if an exception is being handled
+            parameters = None
+            (t, v, tb) = sys.exc_info()
+            if t is not None and v is not None:
+                parameters = '{} {}'.format(t , v)
 
-        # create logging span
-        with tracer.start_active_span('log', child_of=parent_span) as scope:
-            scope.span.log_kv({ 'message': msg })
-            if parameters is not None:
-                scope.span.log_kv({ 'parameters': parameters })
-            # extra tags for an error
-            if argv[0] >= logging.ERROR:
-                scope.span.set_tag('error', True)
-                ec = scope.span.tags.get('ec', 0)
-                scope.span.set_tag('ec', ec + 1)
-
-
-    return wrapped(*argv, **kwargs)
+            # create logging span
+            with tracer.start_active_span('log', child_of=parent_span) as scope:
+                scope.span.log_kv({ 'message': msg })
+                if parameters is not None:
+                    scope.span.log_kv({ 'parameters': parameters })
+                # extra tags for an error
+                if argv[0] >= logging.ERROR:
+                    scope.span.set_tag('error', True)
+                    ec = scope.span.tags.get('ec', 0)
+                    scope.span.set_tag('ec', ec + 1)
+    except Exception as e:
+        logger.debug('Exception: %s', e, exc_info=True)
+    finally:
+        return wrapped(*argv, **kwargs)
 
 logger.debug('Instrumenting logging')
 

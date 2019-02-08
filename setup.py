e8c5b36daa9f431297561cea66750022e2da6d49
Peter Giacomo Lombardo <pglombardo@gmail.com>
2019-02-08 17:39:29 +0100

    Bump package version to 1.8.12

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def check_setuptools():
 check_setuptools()
 
 setup(name='instana',
-      version='1.8.11',
+      version='1.8.12',
       url='https://www.instana.com/',
       project_urls={
           'CI: Travis': 'https://travis-ci.org/instana/python-sensor',

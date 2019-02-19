30d6612bdf1bba23da4acb458a93abd5d5b8fd9d
Peter Giacomo Lombardo <pglombardo@gmail.com>
2019-02-19 15:27:52 +0100

    Add aiohttp to exlude list on Python versions earlier than 3.5.3

--- runtests.py
+++ runtests.py
@@ -4,8 +4,8 @@ from distutils.version import LooseVersion
 
 command_line = [__file__, '--verbose']
 
-if (LooseVersion(sys.version) < LooseVersion('3.5')):
-    command_line.extend(['-e', 'asynqp'])
+if (LooseVersion(sys.version) < LooseVersion('3.5.3')):
+    command_line.extend(['-e', 'asynqp', '-e', 'aiohttp'])
 
 print("Nose arguments: %s" % command_line)
 result = nose.main(argv=command_line)

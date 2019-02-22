0faf776e187d25ee2abf3660f1a172e68d53d4ff
Peter Giacomo Lombardo <pglombardo@gmail.com>
2019-02-22 18:06:55 +0100

    Client safeties

--- instana/instrumentation/aiohttp/client.py
+++ instana/instrumentation/aiohttp/client.py
@@ -13,42 +13,52 @@ try:
     import asyncio
 
     async def stan_request_start(session, trace_config_ctx, params):
-        parent_span = async_tracer.active_span
+        try:
+            parent_span = async_tracer.active_span
 
-        # If we're not tracing, just return
-        if parent_span is None:
-            return
+            # If we're not tracing, just return
+            if parent_span is None:
+                return
 
-        scope = async_tracer.start_active_span("aiohttp", child_of=parent_span)
-        trace_config_ctx.scope = scope
+            scope = async_tracer.start_active_span("aiohttp", child_of=parent_span)
+            trace_config_ctx.scope = scope
 
-        async_tracer.inject(scope.span.context, opentracing.Format.HTTP_HEADERS, params.headers)
+            async_tracer.inject(scope.span.context, opentracing.Format.HTTP_HEADERS, params.headers)
 
-        url = str(params.url)
-        parts = url.split('?')
-        if len(parts) > 1:
-            cleaned_qp = strip_secrets(parts[1], agent.secrets_matcher, agent.secrets_list)
-            scope.span.set_tag("http.params", cleaned_qp)
-        scope.span.set_tag("http.url", parts[0])
-        scope.span.set_tag('http.method', params.method)
+            parts = str(params.url).split('?')
+            if len(parts) > 1:
+                cleaned_qp = strip_secrets(parts[1], agent.secrets_matcher, agent.secrets_list)
+                scope.span.set_tag("http.params", cleaned_qp)
+            scope.span.set_tag("http.url", parts[0])
+            scope.span.set_tag('http.method', params.method)
+        except:
+            logger.debug("stan_request_start", exc_info=True)
 
     async def stan_request_end(session, trace_config_ctx, params):
-        scope = trace_config_ctx.scope
-        scope.span.set_tag('http.status_code', params.response.status)
+        try:
+            scope = trace_config_ctx.scope
+            if scope is not None:
+                scope.span.set_tag('http.status_code', params.response.status)
 
-        if 500 <= params.response.status <= 599:
-            scope.span.set_tag("http.error", params.response.reason)
-            scope.span.set_tag("error", True)
-            ec = scope.span.tags.get('ec', 0)
-            scope.span.set_tag("ec", ec + 1)
+                if 500 <= params.response.status <= 599:
+                    scope.span.set_tag("http.error", params.response.reason)
+                    scope.span.set_tag("error", True)
+                    ec = scope.span.tags.get('ec', 0)
+                    scope.span.set_tag("ec", ec + 1)
 
-        scope.close()
+                scope.close()
+        except:
+            logger.debug("stan_request_end", exc_info=True)
 
     async def stan_request_exception(session, trace_config_ctx, params):
-        scope = trace_config_ctx.scope
-        scope.span.log_exception(params.exception)
-        scope.span.set_tag("http.error", str(params.exception))
-        scope.close()
+        try:
+            scope = trace_config_ctx.scope
+            if scope is not None:
+                scope.span.log_exception(params.exception)
+                scope.span.set_tag("http.error", str(params.exception))
+                scope.close()
+        except:
+            logger.debug("stan_request_exception", exc_info=True)
 
     @wrapt.patch_function_wrapper('aiohttp.client','ClientSession.__init__')
     def init_with_instana(wrapped, instance, argv, kwargs):

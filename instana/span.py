815e5e70cfbee687f56d42c10fed0a149bcf9074
Peter Giacomo Lombardo <pglombardo@gmail.com>
2019-02-22 18:06:14 +0100

    Safer exception msg extraction

--- instana/span.py
+++ instana/span.py
@@ -8,8 +8,11 @@ class InstanaSpan(BasicSpan):
         super(InstanaSpan, self).finish(finish_time)
 
     def log_exception(self, e):
-        if hasattr(e, 'message') and len(e.message):
-            self.log_kv({'message': e.message})
+        if hasattr(e, 'message'):
+            if (e.message is None) or len(e.message) == 0:
+                self.log_kv({'message': e.__class__})
+            else:
+                self.log_kv({'message': e.message})
         elif hasattr(e, '__str__'):
             self.log_kv({'message': e.__str__()})
         else:

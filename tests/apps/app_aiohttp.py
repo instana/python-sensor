00fb4f154a75ed2307381189a56a6f25916877df
Peter Giacomo Lombardo <pglombardo@gmail.com>
2019-02-23 11:36:42 +0100

    More test app routes

--- tests/apps/app_aiohttp.py
+++ tests/apps/app_aiohttp.py
@@ -10,12 +10,22 @@ def say_hello(request):
     return web.Response(text='Hello, world')
 
 
+def four_hundred_one(request):
+    return web.HTTPUnauthorized(reason="I must simulate errors.", text="Simulated server error.")
+
+
+def five_hundred(request):
+    return web.HTTPInternalServerError(reason="I must simulate errors.", text="Simulated server error.")
+
+
 def run_server():
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
 
     app = web.Application(debug=False)
     app.add_routes([web.get('/', say_hello)])
+    app.add_routes([web.get('/401', four_hundred_one)])
+    app.add_routes([web.get('/500', five_hundred)])
 
     runner = web.AppRunner(app)
     loop.run_until_complete(runner.setup())

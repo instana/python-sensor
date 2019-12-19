f55e32dc5c3593e7cc46301aad70002eca822e69
Peter Giacomo Lombardo <pglombardo@gmail.com>
2019-12-19 12:30:26 +0000

    More refinements: add thread disabled warning

--- instana/hooks/hook_uwsgi.py
+++ instana/hooks/hook_uwsgi.py
@@ -10,11 +10,20 @@ from ..singletons import agent
 
 try:
     import uwsgi
-    import uwsgidecorators
+    logger.debug("uWSGI options: %s", uwsgi.opt)
 
-    logger.warn("uWSGI options: %s", uwsgi.opt)
+    opt_master = uwsgi.opt.get('master', False)
+    opt_lazy_apps = uwsgi.opt.get('lazy-apps', False)
 
-    if 'lazy-apps' not in uwsgi.opt or uwsgi.opt['lazy-apps'] is not True:
+    if uwsgi.opt.get('enable-threads', False) is False:
+        logger.warn("Required: uWSGI threads are not enabled.  " +
+                    "Please enable by using the uWSGI --enable-threads option.")
+
+    if opt_master and opt_lazy_apps is False:
+        # --master is supplied in uWSGI options (otherwise uwsgidecorators package won't be available)
+        # When --lazy-apps is True, this postfork hook isn't needed
+        import uwsgidecorators
+
         @uwsgidecorators.postfork
         def uwsgi_handle_fork():
             """ This is our uWSGI hook to detect and act when worker processes are forked off. """
@@ -23,7 +32,7 @@ try:
 
         logger.debug("Applied uWSGI hooks")
     else:
-        logger.debug("uWSGI lazy-apps enabled.  Not applying postfork hooks")
+        logger.debug("uWSGI --master=%s --lazy-apps=%s: postfork hooks not applied", opt_master, opt_lazy_apps)
 except ImportError as e:
-    logger.debug('uwsgi hooks: decorators not available')
+    logger.debug('uwsgi hooks: decorators not available: %s', e)
     pass

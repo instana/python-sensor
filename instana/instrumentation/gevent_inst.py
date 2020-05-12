aadb2bcb093b18feeebc308b01e90e363f025d6c
Peter Giacomo Lombardo <pglombardo@gmail.com>
2020-05-12 13:59:51 +0200

    Use cloned scopes to propagate context

--- instana/instrumentation/gevent_inst.py
+++ instana/instrumentation/gevent_inst.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 import sys
 from ..log import logger
+from opentracing.scope_managers.gevent import _GeventScope
 from ..singletons import agent, tracer
 
 try:
@@ -12,15 +13,13 @@ try:
         from opentracing.scope_managers.gevent import GeventScopeManager
 
         def spawn_callback(gr):
-            current_greenlet = gevent.getcurrent()
             parent_scope = tracer.scope_manager.active
-
-            # logger.debug("current_greenlet: %s", current_greenlet)
-            # logger.debug("other greenlet: %s", gr)
-
-            if (type(current_greenlet) is gevent._greenlet.Greenlet) and parent_scope is not None:
-                parent_scope._finish_on_close = False
-                tracer._scope_manager._set_greenlet_scope(parent_scope, gr)
+            if parent_scope is not None:
+                # New greenlet, new clean slate.  Clone and make active in this new greenlet
+                # the currently active scope (but don't close this span on close - it's a
+                # clone/not the original)
+                parent_scope_clone = _GeventScope(parent_scope.manager, parent_scope.span, finish_on_close=False)
+                tracer._scope_manager._set_greenlet_scope(parent_scope_clone, gr)
 
         logger.debug(" -> Updating tracer to use gevent based context management")
         tracer._scope_manager = GeventScopeManager()

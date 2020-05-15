fb68e82c71b2cafd7748446aa6f6fb32ae242e37
Peter Giacomo Lombardo <pglombardo@gmail.com>
2020-05-15 13:18:51 +0000

    Add tests for imap unordered

--- tests/test_gevent.py
+++ tests/test_gevent.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 import gevent
+from gevent.pool import Group
 import unittest
 import urllib3
 
@@ -21,7 +22,7 @@ class TestGEvent(unittest.TestCase):
         """ Do nothing for now """
         pass
 
-    def make_http_call(self):
+    def make_http_call(self, n=None):
         return self.http.request('GET', testenv["wsgi_server"] + '/')
 
     def spawn_calls(self):
@@ -31,6 +32,13 @@ class TestGEvent(unittest.TestCase):
             jobs.append(gevent.spawn(self.make_http_call))
             jobs.append(gevent.spawn(self.make_http_call))
             gevent.joinall(jobs, timeout=2)
+
+    def spawn_imap_unordered(self):
+        igroup = Group()
+        result = []
+        with tracer.start_active_span('test'):
+            for i in igroup.imap_unordered(self.make_http_call, range(3)):
+                result.append(i)
 
     def launch_gevent_chain(self):
         with tracer.start_active_span('test'):
@@ -70,6 +78,38 @@ class TestGEvent(unittest.TestCase):
             # spans should all have the same test span parent
             self.assertEqual(urllib3_span.t, spawn_span.t)
             self.assertEqual(urllib3_span.p, spawn_span.s)
+
+            # find the wsgi span generated from this urllib3 request
+            span_filter = lambda span: span.n == "wsgi" and span.p == urllib3_span.s
+            wsgi_spans = get_spans_by_filter(spans, span_filter)
+            self.assertIsNotNone(wsgi_spans)
+            self.assertEqual(len(wsgi_spans), 1)
+
+    def test_imap_unordered(self):
+        gevent.spawn(self.spawn_imap_unordered())
+
+        gevent.sleep(2)
+
+        spans = self.recorder.queued_spans()
+        self.assertEqual(7, len(spans))
+
+        span_filter = lambda span: span.n == "sdk" \
+                                   and span.data['sdk']['name'] == 'test' and span.p == None
+        test_spans = get_spans_by_filter(spans, span_filter)
+        self.assertIsNotNone(test_spans)
+        self.assertEqual(len(test_spans), 1)
+
+        test_span = test_spans[0]
+        self.assertTrue(type(test_spans[0]) is SDKSpan)
+
+        span_filter = lambda span: span.n == "urllib3"
+        urllib3_spans = get_spans_by_filter(spans, span_filter)
+        self.assertEqual(len(urllib3_spans), 3)
+
+        for urllib3_span in urllib3_spans:
+            # spans should all have the same test span parent
+            self.assertEqual(urllib3_span.t, test_span.t)
+            self.assertEqual(urllib3_span.p, test_span.s)
 
             # find the wsgi span generated from this urllib3 request
             span_filter = lambda span: span.n == "wsgi" and span.p == urllib3_span.s

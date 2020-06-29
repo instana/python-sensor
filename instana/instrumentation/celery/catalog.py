c66df2535a05118107996ac28f4cd69679da903d
Peter Giacomo Lombardo <pglombardo@gmail.com>
2020-06-29 13:24:41 +0200

    Method docs and task_catalog_get

--- instana/instrumentation/celery/catalog.py
+++ instana/instrumentation/celery/catalog.py
@@ -13,6 +13,9 @@ from weakref import WeakValueDictionary
 
 
 def get_task_id(headers, body):
+    """
+    Across Celery versions, the task id can exist in a couple of places.
+    """
     id = headers.get('id', None)
     if id is None:
         id = body.get('id', None)
@@ -20,6 +23,13 @@ def get_task_id(headers, body):
 
 
 def task_catalog_push(task, task_id, scope, is_consumer):
+    """
+    Push (adds) an object to the task catalog
+    @param task: The Celery Task
+    @param task_id:  The Celery Task ID
+    @param is_consumer: Boolean
+    @return: scope
+    """
     catalog = None
     if not hasattr(task, '_instana_scopes'):
         catalog = WeakValueDictionary()
@@ -32,9 +42,33 @@ def task_catalog_push(task, task_id, scope, is_consumer):
 
 
 def task_catalog_pop(task, task_id, is_consumer):
+    """
+    Pop (removes) an object from the task catalog
+    @param task: The Celery Task
+    @param task_id:  The Celery Task ID
+    @param is_consumer: Boolean
+    @return: scope
+    """
     catalog = getattr(task, '_instana_scopes', None)
     if catalog is None:
         return None
 
     key = (task_id, is_consumer)
     return catalog.pop(key, None)
+
+
+def task_catalog_get(task, task_id, is_consumer):
+    """
+    Get an object from the task catalog
+    @param task: The Celery Task
+    @param task_id:  The Celery Task ID
+    @param is_consumer: Boolean
+    @return: scope
+    """
+    catalog = getattr(task, '_instana_scopes', None)
+    if catalog is None:
+        return None
+
+    key = (task_id, is_consumer)
+    return catalog.get(key, None)
+

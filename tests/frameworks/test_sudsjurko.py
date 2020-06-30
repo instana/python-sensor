fecf7973dbd2e3ee94a5ca38717fa9de309b0510
Peter Giacomo Lombardo <pglombardo@gmail.com>
2020-06-30 10:04:16 +0000

    Remove deprecated assert_equals

--- tests/frameworks/test_sudsjurko.py
+++ tests/frameworks/test_sudsjurko.py
@@ -1,14 +1,14 @@
 from __future__ import absolute_import
 
+import unittest
 import tests.apps.soap_app
 from ..helpers import testenv
 from suds.client import Client
-from nose.tools import assert_equals
 from instana.singletons import tracer
 
 
 
-class TestSudsJurko:
+class TestSudsJurko(unittest.TestCase):
     def setup_class(self):
         """ Clear all spans before a test run """
         self.client = Client(testenv["soap_server"] + '/?wsdl', cache=None)
@@ -21,12 +21,12 @@ class TestSudsJurko:
     def test_vanilla_request(self):
         response = self.client.service.ask_question(u'Why u like dat?', 5)
 
-        assert_equals(1, len(response))
-        assert_equals(1, len(response[0]))
+        self.assertEquals(1, len(response))
+        self.assertEquals(1, len(response[0]))
         assert(type(response[0]) is list)
 
         spans = self.recorder.queued_spans()
-        assert_equals(1, len(spans))
+        self.assertEquals(1, len(spans))
 
     def test_basic_request(self):
         with tracer.start_active_span('test'):
@@ -34,25 +34,25 @@ class TestSudsJurko:
 
         spans = self.recorder.queued_spans()
 
-        assert_equals(3, len(spans))
+        self.assertEquals(3, len(spans))
         wsgi_span = spans[0]
         soap_span = spans[1]
         test_span = spans[2]
 
-        assert_equals(1, len(response))
-        assert_equals(1, len(response[0]))
+        self.assertEquals(1, len(response))
+        self.assertEquals(1, len(response[0]))
         assert(type(response[0]) is list)
 
-        assert_equals("test", test_span.data["sdk"]["name"])
-        assert_equals(test_span.t, soap_span.t)
-        assert_equals(soap_span.p, test_span.s)
-        assert_equals(wsgi_span.t, soap_span.t)
-        assert_equals(wsgi_span.p, soap_span.s)
+        self.assertEquals("test", test_span.data["sdk"]["name"])
+        self.assertEquals(test_span.t, soap_span.t)
+        self.assertEquals(soap_span.p, test_span.s)
+        self.assertEquals(wsgi_span.t, soap_span.t)
+        self.assertEquals(wsgi_span.p, soap_span.s)
 
-        assert_equals(None, soap_span.ec)
+        self.assertEquals(None, soap_span.ec)
 
-        assert_equals('ask_question', soap_span.data["soap"]["action"])
-        assert_equals(testenv["soap_server"] + '/', soap_span.data["http"]["url"])
+        self.assertEquals('ask_question', soap_span.data["soap"]["action"])
+        self.assertEquals(testenv["soap_server"] + '/', soap_span.data["http"]["url"])
 
     def test_server_exception(self):
         response = None
@@ -63,7 +63,7 @@ class TestSudsJurko:
                 pass
 
         spans = self.recorder.queued_spans()
-        assert_equals(5, len(spans))
+        self.assertEquals(5, len(spans))
 
         log_span1 = spans[0]
         wsgi_span = spans[1]
@@ -71,17 +71,17 @@ class TestSudsJurko:
         soap_span = spans[3]
         test_span = spans[4]
 
-        assert_equals(None, response)
-        assert_equals("test", test_span.data["sdk"]["name"])
-        assert_equals(test_span.t, soap_span.t)
-        assert_equals(soap_span.p, test_span.s)
-        assert_equals(wsgi_span.t, soap_span.t)
-        assert_equals(wsgi_span.p, soap_span.s)
+        self.assertEquals(None, response)
+        self.assertEquals("test", test_span.data["sdk"]["name"])
+        self.assertEquals(test_span.t, soap_span.t)
+        self.assertEquals(soap_span.p, test_span.s)
+        self.assertEquals(wsgi_span.t, soap_span.t)
+        self.assertEquals(wsgi_span.p, soap_span.s)
 
-        assert_equals(1, soap_span.ec)
-        assert_equals(u"Server raised fault: 'Internal Error'", soap_span.data["http"]["error"])
-        assert_equals('server_exception', soap_span.data["soap"]["action"])
-        assert_equals(testenv["soap_server"] + '/', soap_span.data["http"]["url"])
+        self.assertEquals(1, soap_span.ec)
+        self.assertEquals(u"Server raised fault: 'Internal Error'", soap_span.data["http"]["error"])
+        self.assertEquals('server_exception', soap_span.data["soap"]["action"])
+        self.assertEquals(testenv["soap_server"] + '/', soap_span.data["http"]["url"])
 
     def test_server_fault(self):
         response = None
@@ -92,24 +92,24 @@ class TestSudsJurko:
                 pass
 
         spans = self.recorder.queued_spans()
-        assert_equals(5, len(spans))
+        self.assertEquals(5, len(spans))
         log_span1 = spans[0]
         wsgi_span = spans[1]
         log_span2 = spans[2]
         soap_span = spans[3]
         test_span = spans[4]
 
-        assert_equals(None, response)
-        assert_equals("test", test_span.data["sdk"]["name"])
-        assert_equals(test_span.t, soap_span.t)
-        assert_equals(soap_span.p, test_span.s)
-        assert_equals(wsgi_span.t, soap_span.t)
-        assert_equals(wsgi_span.p, soap_span.s)
+        self.assertEquals(None, response)
+        self.assertEquals("test", test_span.data["sdk"]["name"])
+        self.assertEquals(test_span.t, soap_span.t)
+        self.assertEquals(soap_span.p, test_span.s)
+        self.assertEquals(wsgi_span.t, soap_span.t)
+        self.assertEquals(wsgi_span.p, soap_span.s)
 
-        assert_equals(1, soap_span.ec)
-        assert_equals(u"Server raised fault: 'Server side fault example.'", soap_span.data["http"]["error"])
-        assert_equals('server_fault', soap_span.data["soap"]["action"])
-        assert_equals(testenv["soap_server"] + '/', soap_span.data["http"]["url"])
+        self.assertEquals(1, soap_span.ec)
+        self.assertEquals(u"Server raised fault: 'Server side fault example.'", soap_span.data["http"]["error"])
+        self.assertEquals('server_fault', soap_span.data["soap"]["action"])
+        self.assertEquals(testenv["soap_server"] + '/', soap_span.data["http"]["url"])
 
     def test_client_fault(self):
         response = None
@@ -120,7 +120,7 @@ class TestSudsJurko:
                 pass
 
         spans = self.recorder.queued_spans()
-        assert_equals(5, len(spans))
+        self.assertEquals(5, len(spans))
 
         log_span1 = spans[0]
         wsgi_span = spans[1]
@@ -128,14 +128,14 @@ class TestSudsJurko:
         soap_span = spans[3]
         test_span = spans[4]
 
-        assert_equals(None, response)
-        assert_equals("test", test_span.data["sdk"]["name"])
-        assert_equals(test_span.t, soap_span.t)
-        assert_equals(soap_span.p, test_span.s)
-        assert_equals(wsgi_span.t, soap_span.t)
-        assert_equals(wsgi_span.p, soap_span.s)
+        self.assertEquals(None, response)
+        self.assertEquals("test", test_span.data["sdk"]["name"])
+        self.assertEquals(test_span.t, soap_span.t)
+        self.assertEquals(soap_span.p, test_span.s)
+        self.assertEquals(wsgi_span.t, soap_span.t)
+        self.assertEquals(wsgi_span.p, soap_span.s)
 
-        assert_equals(1, soap_span.ec)
-        assert_equals(u"Server raised fault: 'Client side fault example'", soap_span.data["http"]["error"])
-        assert_equals('client_fault', soap_span.data["soap"]["action"])
-        assert_equals(testenv["soap_server"] + '/', soap_span.data["http"]["url"])
+        self.assertEquals(1, soap_span.ec)
+        self.assertEquals(u"Server raised fault: 'Client side fault example'", soap_span.data["http"]["error"])
+        self.assertEquals('client_fault', soap_span.data["soap"]["action"])
+        self.assertEquals(testenv["soap_server"] + '/', soap_span.data["http"]["url"])

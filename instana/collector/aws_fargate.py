1b1b24695fe78c89e11f99a32f8633b3a97939bc
Peter Giacomo Lombardo <nosis@rbx.run>
2020-07-24 08:56:24 +0000

    Check existence before referencing

--- instana/collector/aws_fargate.py
+++ instana/collector/aws_fargate.py
@@ -157,21 +157,22 @@ class AWSFargateCollector(BaseCollector):
         """
         plugin_data = dict()
         try:
-            plugin_data["name"] = "com.instana.plugin.aws.ecs.task"
-            plugin_data["entityId"] = "metadata.TaskARN" # FIXME
-            plugin_data["data"] = DictionaryOfStan()
-            plugin_data["data"]["taskArn"] = self.task_metadata.get("TaskARN", None)
-            plugin_data["data"]["clusterArn"] = self.task_metadata.get("Cluster", None)
-            plugin_data["data"]["taskDefinition"] = self.task_metadata.get("Family", None)
-            plugin_data["data"]["taskDefinitionVersion"] = self.task_metadata.get("Revision", None)
-            plugin_data["data"]["availabilityZone"] = self.task_metadata.get("AvailabilityZone", None)
-            plugin_data["data"]["desiredStatus"] = self.task_metadata.get("DesiredStatus", None)
-            plugin_data["data"]["knownStatus"] = self.task_metadata.get("KnownStatus", None)
-            plugin_data["data"]["pullStartedAt"] = self.task_metadata.get("PullStartedAt", None)
-            plugin_data["data"]["pullStoppedAt"] = self.task_metadata.get("PullStoppeddAt", None)
-            limits = self.task_metadata.get("Limits", {})
-            plugin_data["data"]["limits"]["cpu"] = limits.get("CPU", None)
-            plugin_data["data"]["limits"]["memory"] = limits.get("Memory", None)
+            if self.task_metadata is not None:
+                plugin_data["name"] = "com.instana.plugin.aws.ecs.task"
+                plugin_data["entityId"] = "metadata.TaskARN" # FIXME
+                plugin_data["data"] = DictionaryOfStan()
+                plugin_data["data"]["taskArn"] = self.task_metadata.get("TaskARN", None)
+                plugin_data["data"]["clusterArn"] = self.task_metadata.get("Cluster", None)
+                plugin_data["data"]["taskDefinition"] = self.task_metadata.get("Family", None)
+                plugin_data["data"]["taskDefinitionVersion"] = self.task_metadata.get("Revision", None)
+                plugin_data["data"]["availabilityZone"] = self.task_metadata.get("AvailabilityZone", None)
+                plugin_data["data"]["desiredStatus"] = self.task_metadata.get("DesiredStatus", None)
+                plugin_data["data"]["knownStatus"] = self.task_metadata.get("KnownStatus", None)
+                plugin_data["data"]["pullStartedAt"] = self.task_metadata.get("PullStartedAt", None)
+                plugin_data["data"]["pullStoppedAt"] = self.task_metadata.get("PullStoppeddAt", None)
+                limits = self.task_metadata.get("Limits", {})
+                plugin_data["data"]["limits"]["cpu"] = limits.get("CPU", None)
+                plugin_data["data"]["limits"]["memory"] = limits.get("Memory", None)
         except:
             logger.debug("_collect_task_snapshot: ", exc_info=True)
         return [plugin_data]
@@ -183,44 +184,45 @@ class AWSFargateCollector(BaseCollector):
         """
         plugins = []
         try:
-            containers = self.task_metadata.get("Containers", [])
-            for container in containers:
-                plugin_data = dict()
-                try:
-                    labels = container.get("Labels", {})
-                    name = container.get("Name", "")
-                    taskArn = labels.get("com.amazonaws.ecs.container-name", "")
+            if self.task_metadata is not None:
+                containers = self.task_metadata.get("Containers", [])
+                for container in containers:
+                    plugin_data = dict()
+                    try:
+                        labels = container.get("Labels", {})
+                        name = container.get("Name", "")
+                        taskArn = labels.get("com.amazonaws.ecs.container-name", "")
 
-                    plugin_data["name"] = "com.instana.plugin.aws.ecs.container"
-                    # "entityId": $taskARN + "::" + $containerName
-                    plugin_data["entityId"] = "%s::%s" % (taskArn, name)
+                        plugin_data["name"] = "com.instana.plugin.aws.ecs.container"
+                        # "entityId": $taskARN + "::" + $containerName
+                        plugin_data["entityId"] = "%s::%s" % (taskArn, name)
 
-                    plugin_data["data"] = DictionaryOfStan()
-                    if self.root_metadata["Name"] == name:
-                        plugin_data["data"]["instrumented"] = True
-                    plugin_data["data"]["runtime"] = "python"
-                    plugin_data["data"]["dockerId"] = container.get("DockerId", None)
-                    plugin_data["data"]["dockerName"] = container.get("DockerName", None)
-                    plugin_data["data"]["containerName"] = container.get("Name", None)
-                    plugin_data["data"]["image"] = container.get("Image", None)
-                    plugin_data["data"]["imageId"] = container.get("ImageID", None)
-                    plugin_data["data"]["taskArn"] = labels.get("com.amazonaws.ecs.task-arn", None)
-                    plugin_data["data"]["taskDefinition"] = labels.get("com.amazonaws.ecs.task-definition-family", None)
-                    plugin_data["data"]["taskDefinitionVersion"] = labels.get("com.amazonaws.ecs.task-definition-version", None)
-                    plugin_data["data"]["clusterArn"] = labels.get("com.amazonaws.ecs.cluster", None)
-                    plugin_data["data"]["desiredStatus"] = container.get("DesiredStatus", None)
-                    plugin_data["data"]["knownStatus"] = container.get("KnownStatus", None)
-                    plugin_data["data"]["ports"] = container.get("Ports", None)
-                    plugin_data["data"]["createdAt"] = container.get("CreatedAt", None)
-                    plugin_data["data"]["startedAt"] = container.get("StartedAt", None)
-                    plugin_data["data"]["type"] = container.get("Type", None)
-                    limits = container.get("Limits", {})
-                    plugin_data["data"]["limits"]["cpu"] = limits.get("CPU", None)
-                    plugin_data["data"]["limits"]["memory"] = limits.get("Memory", None)
-                except:
-                    logger.debug("_collect_container_snapshots: ", exc_info=True)
-                finally:
-                    plugins.append(plugin_data)
+                        plugin_data["data"] = DictionaryOfStan()
+                        if self.root_metadata["Name"] == name:
+                            plugin_data["data"]["instrumented"] = True
+                        plugin_data["data"]["runtime"] = "python"
+                        plugin_data["data"]["dockerId"] = container.get("DockerId", None)
+                        plugin_data["data"]["dockerName"] = container.get("DockerName", None)
+                        plugin_data["data"]["containerName"] = container.get("Name", None)
+                        plugin_data["data"]["image"] = container.get("Image", None)
+                        plugin_data["data"]["imageId"] = container.get("ImageID", None)
+                        plugin_data["data"]["taskArn"] = labels.get("com.amazonaws.ecs.task-arn", None)
+                        plugin_data["data"]["taskDefinition"] = labels.get("com.amazonaws.ecs.task-definition-family", None)
+                        plugin_data["data"]["taskDefinitionVersion"] = labels.get("com.amazonaws.ecs.task-definition-version", None)
+                        plugin_data["data"]["clusterArn"] = labels.get("com.amazonaws.ecs.cluster", None)
+                        plugin_data["data"]["desiredStatus"] = container.get("DesiredStatus", None)
+                        plugin_data["data"]["knownStatus"] = container.get("KnownStatus", None)
+                        plugin_data["data"]["ports"] = container.get("Ports", None)
+                        plugin_data["data"]["createdAt"] = container.get("CreatedAt", None)
+                        plugin_data["data"]["startedAt"] = container.get("StartedAt", None)
+                        plugin_data["data"]["type"] = container.get("Type", None)
+                        limits = container.get("Limits", {})
+                        plugin_data["data"]["limits"]["cpu"] = limits.get("CPU", None)
+                        plugin_data["data"]["limits"]["memory"] = limits.get("Memory", None)
+                    except:
+                        logger.debug("_collect_container_snapshots: ", exc_info=True)
+                    finally:
+                        plugins.append(plugin_data)
         except:
             logger.debug("_collect_container_snapshots: ", exc_info=True)
         return plugins
@@ -228,32 +230,33 @@ class AWSFargateCollector(BaseCollector):
     def _collect_docker_snapshot(self):
         plugins = []
         try:
-            containers = self.task_metadata.get("Containers", [])
-            for container in containers:
-                plugin_data = dict()
-                try:
-                    labels = container.get("Labels", {})
-                    name = container.get("Name", "")
-                    taskArn = labels.get("com.amazonaws.ecs.container-name", "")
+            if self.task_metadata is not None:
+                containers = self.task_metadata.get("Containers", [])
+                for container in containers:
+                    plugin_data = dict()
+                    try:
+                        labels = container.get("Labels", {})
+                        name = container.get("Name", "")
+                        taskArn = labels.get("com.amazonaws.ecs.container-name", "")
 
-                    plugin_data["name"] = "com.instana.plugin.docker"
-                    # "entityId": $taskARN + "::" + $containerName
-                    plugin_data["entityId"] = "%s::%s" % (taskArn, name)
-                    plugin_data["data"] = DictionaryOfStan()
-                    plugin_data["data"]["Id"] = container.get("DockerId", None)
-                    plugin_data["data"]["Created"] = container.get("CreatedAt", None)
-                    plugin_data["data"]["Started"] = container.get("StartedAt", None)
-                    plugin_data["data"]["Image"] = container.get("Image", None)
-                    plugin_data["data"]["Labels"] = container.get("Labels", None)
-                    plugin_data["data"]["Ports"] = container.get("Ports", None)
+                        plugin_data["name"] = "com.instana.plugin.docker"
+                        # "entityId": $taskARN + "::" + $containerName
+                        plugin_data["entityId"] = "%s::%s" % (taskArn, name)
+                        plugin_data["data"] = DictionaryOfStan()
+                        plugin_data["data"]["Id"] = container.get("DockerId", None)
+                        plugin_data["data"]["Created"] = container.get("CreatedAt", None)
+                        plugin_data["data"]["Started"] = container.get("StartedAt", None)
+                        plugin_data["data"]["Image"] = container.get("Image", None)
+                        plugin_data["data"]["Labels"] = container.get("Labels", None)
+                        plugin_data["data"]["Ports"] = container.get("Ports", None)
 
-                    networks = container.get("Networks", [])
-                    if len(networks) >= 1:
-                        plugin_data["data"]["NetworkMode"] = networks[0].get("NetworkMode", None)
-                except:
-                    logger.debug("_collect_container_snapshots: ", exc_info=True)
-                finally:
-                    plugins.append(plugin_data)
+                        networks = container.get("Networks", [])
+                        if len(networks) >= 1:
+                            plugin_data["data"]["NetworkMode"] = networks[0].get("NetworkMode", None)
+                    except:
+                        logger.debug("_collect_container_snapshots: ", exc_info=True)
+                    finally:
+                        plugins.append(plugin_data)
         except:
             logger.debug("_collect_container_snapshots: ", exc_info=True)
         return plugins

d6840713f34c7b8a21596e76f17deb92f3784ab8
Andrey Slotin <andrey.slotin@instana.com>
2020-09-24 16:53:02 +0200

    Add trace context propagation test util

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -22,3 +22,8 @@ def test_validate_url():
     assert(validate_url("http:boligrafo") is False)
     assert(validate_url(None) is False)
 
+class _TraceContextMixin:
+    def assertTraceContextPropagated(self, parent_span, child_span):
+        self.assertEqual(parent_span.t, child_span.t)
+        self.assertEqual(parent_span.s, child_span.p)
+        self.assertNotEqual(parent_span.s, child_span.s)

4e6e143a5e430e4a03c8845984626cc9a65440b8
Peter Giacomo Lombardo <nosis@rbx.run>
2020-10-21 12:27:21 +0200

    Tests: Support launching threads with args

--- tests/apps/utils.py
+++ tests/apps/utils.py
@@ -1,10 +1,12 @@
 import threading
 
 
-def launch_background_thread(app, name):
-    app_thread = threading.Thread(target=app)
-    app_thread.daemon = True
-    app_thread.name = "Background %s app" % name
-    print("Starting background %s app..." % name)
+def launch_background_thread(app, app_name, fun_args=(), fun_kwargs={}):
+    print("Starting background %s app..." % app_name)
+    app_thread = threading.Thread(target=app,
+                                  daemon=True,
+                                  name=app_name,
+                                  args=fun_args,
+                                  kwargs=fun_kwargs)
     app_thread.start()
     return app_thread

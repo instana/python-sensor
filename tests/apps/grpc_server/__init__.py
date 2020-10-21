a763deebdc39a0b7407b0c67affcf203ff307098
Peter Giacomo Lombardo <nosis@rbx.run>
2020-10-21 12:26:31 +0200

    Load GRPC server on-demand only

--- tests/apps/grpc_server/__init__.py
+++ tests/apps/grpc_server/__init__.py
@@ -1,1 +1,19 @@
-# __all__ = ["digestor_pb2", "digestor_pb2_grpc"]
+import os
+import sys
+import time
+import threading
+
+if 'GEVENT_TEST' not in os.environ and 'CASSANDRA_TEST' not in os.environ and sys.version_info >= (3, 5, 3):
+    # Background RPC application
+    #
+    # Spawn the background RPC app that the tests will throw
+    # requests at.
+    import tests.apps.grpc_server
+    from .stan_server import StanServicer
+    stan_servicer = StanServicer()
+    rpc_server_thread = threading.Thread(target=stan_servicer.start_server)
+    rpc_server_thread.daemon = True
+    rpc_server_thread.name = "Background RPC app"
+    print("Starting background RPC app...")
+    rpc_server_thread.start()
+    time.sleep(1)

204810b01b2f282e5ccf9e7156e520d2f6cafebd
Peter Giacomo Lombardo <nosis@rbx.run>
2020-10-22 14:51:26 +0200

    Python 2 compatibility

--- tests/apps/utils.py
+++ tests/apps/utils.py
@@ -4,9 +4,9 @@ import threading
 def launch_background_thread(app, app_name, fun_args=(), fun_kwargs={}):
     print("Starting background %s app..." % app_name)
     app_thread = threading.Thread(target=app,
-                                  daemon=True,
                                   name=app_name,
                                   args=fun_args,
                                   kwargs=fun_kwargs)
+    app_thread.daemon = True
     app_thread.start()
     return app_thread

a5533a3e6344bad6bf63fa84670394c78b02024e
Peter Giacomo Lombardo <nosis@rbx.run>
2020-10-27 17:13:41 +0100

    Set custom header options before launching background process

--- tests/apps/fastapi_app/__init__.py
+++ tests/apps/fastapi_app/__init__.py
@@ -9,7 +9,12 @@ APP_THREAD = None
 
 def launch_fastapi(mp_queue):
     from instana.singletons import agent
+
+    # Use the multiprocess queue
     agent.collector.span_queue = mp_queue
+    # Hack together a manual custom headers list; We'll use this in tests
+    agent.options.extra_http_headers = [u'X-Capture-This', u'X-Capture-That']
+
     logger.warning('Child PID: %s', os.getpid())
     logger.warning('Child mp_queue: %s', mp_queue)
     uvicorn.run(fastapi_server, host='127.0.0.1', port=testenv['fastapi_port'])

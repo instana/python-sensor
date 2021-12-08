60604bde2d4ecae755532246298063780a33beab
dimitraparaskevopoulou <dimitra.paraskevopoulou@instana.com>
2021-12-08 08:47:14 +0100

    test latest version of cassandra

--- instana/instrumentation/cassandra_inst.py
+++ instana/instrumentation/cassandra_inst.py
@@ -47,7 +47,7 @@ try:
 
     def cb_request_error(results, span, fn):
         collect_response(span, fn)
-        logger.error(results, results.message)
+        logger.error(results)
         span.mark_as_errored({"cassandra.error": results.message})
         span.finish()
 

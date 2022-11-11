17c439b37417a7cb23600441d6e7ea5e5e9c00ad
Ferenc Géczi <ferenc.geczi@ibm.com>
2022-11-11 10:00:00 +0000

    workaround: Hardcode predicted branch around environment
    Signed-off-by: Ferenc Géczi <ferenc.geczi@ibm.com>

--- instana/singletons.py
+++ instana/singletons.py
@@ -15,51 +15,51 @@ async_tracer = None
 profiler = None
 span_recorder = None
 
-# Detect the environment where we are running ahead of time
-aws_env = os.environ.get("AWS_EXECUTION_ENV", "")
-env_is_test = "INSTANA_TEST" in os.environ
-env_is_aws_fargate = aws_env == "AWS_ECS_FARGATE"
-env_is_aws_lambda = "AWS_Lambda_" in aws_env
-k_service = os.environ.get("K_SERVICE")
-k_configuration = os.environ.get("K_CONFIGURATION")
-k_revision = os.environ.get("K_REVISION")
-instana_endpoint_url = os.environ.get("INSTANA_ENDPOINT_URL")
-env_is_google_cloud_run = all((k_service, k_configuration, k_revision, instana_endpoint_url))
+## Detect the environment where we are running ahead of time
+#aws_env = os.environ.get("AWS_EXECUTION_ENV", "")
+#env_is_test = "INSTANA_TEST" in os.environ
+#env_is_aws_fargate = aws_env == "AWS_ECS_FARGATE"
+#env_is_aws_lambda = "AWS_Lambda_" in aws_env
+#k_service = os.environ.get("K_SERVICE")
+#k_configuration = os.environ.get("K_CONFIGURATION")
+#k_revision = os.environ.get("K_REVISION")
+#instana_endpoint_url = os.environ.get("INSTANA_ENDPOINT_URL")
+#env_is_google_cloud_run = all((k_service, k_configuration, k_revision, instana_endpoint_url))
+#
+#if env_is_test:
+#    from .agent.test import TestAgent
+#    from .recorder import StanRecorder
+#
+#    agent = TestAgent()
+#    span_recorder = StanRecorder(agent)
+#    profiler = Profiler(agent)
+#
+#elif env_is_aws_lambda:
+#    from .agent.aws_lambda import AWSLambdaAgent
+#    from .recorder import StanRecorder
+#
+#    agent = AWSLambdaAgent()
+#    span_recorder = StanRecorder(agent)
+#
+#elif env_is_aws_fargate:
+#    from .agent.aws_fargate import AWSFargateAgent
+#    from .recorder import StanRecorder
+#
+#    agent = AWSFargateAgent()
+#    span_recorder = StanRecorder(agent)
+#elif env_is_google_cloud_run:
+#    from instana.agent.google_cloud_run import GCRAgent
+#    from instana.recorder import StanRecorder
+#
+#    agent = GCRAgent(service=k_service, configuration=k_configuration, revision=k_revision)
+#    span_recorder = StanRecorder(agent)
+#else:
+from .agent.host import HostAgent
+from .recorder import StanRecorder
 
-if env_is_test:
-    from .agent.test import TestAgent
-    from .recorder import StanRecorder
-
-    agent = TestAgent()
-    span_recorder = StanRecorder(agent)
-    profiler = Profiler(agent)
-
-elif env_is_aws_lambda:
-    from .agent.aws_lambda import AWSLambdaAgent
-    from .recorder import StanRecorder
-
-    agent = AWSLambdaAgent()
-    span_recorder = StanRecorder(agent)
-
-elif env_is_aws_fargate:
-    from .agent.aws_fargate import AWSFargateAgent
-    from .recorder import StanRecorder
-
-    agent = AWSFargateAgent()
-    span_recorder = StanRecorder(agent)
-elif env_is_google_cloud_run:
-    from instana.agent.google_cloud_run import GCRAgent
-    from instana.recorder import StanRecorder
-
-    agent = GCRAgent(service=k_service, configuration=k_configuration, revision=k_revision)
-    span_recorder = StanRecorder(agent)
-else:
-    from .agent.host import HostAgent
-    from .recorder import StanRecorder
-
-    agent = HostAgent()
-    span_recorder = StanRecorder(agent)
-    profiler = Profiler(agent)
+agent = HostAgent()
+span_recorder = StanRecorder(agent)
+profiler = Profiler(agent)
 
 
 def get_agent():

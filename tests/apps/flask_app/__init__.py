458b470aa7bbd51371a0aff7d4b22627b509150d
Ferenc Géczi <ferenc.geczi@ibm.com>
2022-11-21 10:00:00 +0000

    test: Use falsy values for test environment variable in flask_app
    Signed-off-by: Ferenc Géczi <ferenc.geczi@ibm.com>

--- tests/apps/flask_app/__init__.py
+++ tests/apps/flask_app/__init__.py
@@ -7,5 +7,5 @@ from ..utils import launch_background_thread
 
 app_thread = None
 
-if not os.environ('CASSANDRA_TEST') and app_thread is None:
+if not os.environ.get('CASSANDRA_TEST') and app_thread is None:
     app_thread = launch_background_thread(server.serve_forever, "Flask")

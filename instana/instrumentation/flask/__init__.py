7b8239a3892ac66dadb69132d72597615b9518ed
Ferenc Géczi <ferenc.geczi@ibm.com>
2023-09-29 12:00:00 +0000

    fix(flask): 2.3 deprecated the 'signals_available' and 2.4 will remove it
    Signed-off-by: Ferenc Géczi <ferenc.geczi@ibm.com>

--- instana/instrumentation/flask/__init__.py
+++ instana/instrumentation/flask/__init__.py
@@ -5,13 +5,21 @@ from __future__ import absolute_import
 
 try:
     import flask
-    from flask.signals import signals_available
 
     # `signals_available` indicates whether the Flask process is running with or without blinker support:
     # https://pypi.org/project/blinker/
     #
     # Blinker support is preferred but we do the best we can when it's not available.
     #
+    flask_version = tuple(map(int, flask.__version__.split('.')))
+    if flask_version < (2, 3, 0):
+      from flask.signals import signals_available
+    else:
+      # Beginning from 2.3.0 as stated in the notes
+      # https://flask.palletsprojects.com/en/2.3.x/changes/#version-2-3-0
+      # "Signals are always available. blinker>=1.6.2 is a required dependency.
+      # The signals_available attribute is deprecated. #5056"
+      signals_available = True
 
     from . import common
 

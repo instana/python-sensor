24e0fcd19f6ba65034fbca3c3acbc31b8e089462
Paulo Vital <paulo.vital@ibm.com>
2024-02-12 08:32:57 -0800

    refactor: Use ContextVarsScopeManager instead of AsyncioScopeManager.
    As we support now only the Python runtime >= 3.7, the usage of the ContextVarsScopeManager as Tracer's scope manager is indicated to provide automatic Span propagation from parent coroutines, tasks and scheduled in event loop callbacks to their children.
    
    Signed-off-by: Paulo Vital <paulo.vital@ibm.com>

--- instana/instrumentation/asyncio.py
+++ instana/instrumentation/asyncio.py
@@ -7,6 +7,8 @@ import wrapt
 from ..log import logger
 from ..singletons import async_tracer
 from ..configurator import config
+from opentracing.scope_managers.contextvars import no_parent_scope
+from opentracing.scope_managers.constants import ACTIVE_ATTR
 
 try:
     import asyncio
@@ -14,13 +16,14 @@ try:
     @wrapt.patch_function_wrapper('asyncio','ensure_future')
     def ensure_future_with_instana(wrapped, instance, argv, kwargs):
         if config['asyncio_task_context_propagation']['enabled'] is False:
-            return wrapped(*argv, **kwargs)
+            with no_parent_scope():
+                return wrapped(*argv, **kwargs)
 
         scope = async_tracer.scope_manager.active
         task = wrapped(*argv, **kwargs)
 
         if scope is not None:
-            async_tracer.scope_manager._set_task_scope(scope, task=task)
+            setattr(task, ACTIVE_ATTR, scope)
 
         return task
 
@@ -28,13 +31,14 @@ try:
         @wrapt.patch_function_wrapper('asyncio','create_task')
         def create_task_with_instana(wrapped, instance, argv, kwargs):
             if config['asyncio_task_context_propagation']['enabled'] is False:
-                return wrapped(*argv, **kwargs)
+                with no_parent_scope():
+                    return wrapped(*argv, **kwargs)
 
             scope = async_tracer.scope_manager.active
             task = wrapped(*argv, **kwargs)
 
             if scope is not None:
-                async_tracer.scope_manager._set_task_scope(scope, task=task)
+                setattr(task, ACTIVE_ATTR, scope)
 
             return task
 

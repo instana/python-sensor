31d84da792388d235c8a73d7d104ce372172a3e7
Varsha GS <varsha.gs@ibm.com>
2024-05-07 11:35:22 +0530

    chore: add newline to EOF
    Signed-off-by: Varsha GS <varsha.gs@ibm.com>

--- .tekton/.currency/scripts/get-tekton-ci-output.sh
+++ .tekton/.currency/scripts/get-tekton-ci-output.sh
@@ -6,13 +6,11 @@ successful_taskruns=( $(kubectl get taskrun --sort-by=.metadata.creationTimestam
 
 for ((i=${#successful_taskruns[@]}-1; i>=0; i--)); do
     pod_name=$(kubectl get taskrun "${successful_taskruns[$i]}" -o jsonpath='{.status.podName}')
-    ci_output=$(kubectl logs $pod_name -c step-unittest | grep "Successfully installed")
+    ci_output=$(kubectl logs ${pod_name} -c step-unittest | grep "Successfully installed")
     if [ -n "${ci_output}" ]; then
-        latest_successful_taskrun=$successful_taskrun
         latest_successful_taskrun_pod=$pod_name
         break
     fi
 done
-
-echo $latest_successful_taskrun    
-kubectl logs $latest_successful_taskrun_pod -c step-unittest | grep "Successfully installed" > ${TEKTON_CI_OUT_FILE}
+  
+kubectl logs ${latest_successful_taskrun_pod} -c step-unittest | grep "Successfully installed" > ${TEKTON_CI_OUT_FILE}

69bb3e26a36304ba557153b3a6178fbdc8468ac7
Varsha GS <varsha.gs@ibm.com>
2024-05-12 21:47:08 +0530

    currency: use python kubernetes client to interact with the tekton cluster
    Signed-off-by: Varsha GS <varsha.gs@ibm.com>

--- .tekton/.currency/scripts/generate_report.py
+++ .tekton/.currency/scripts/generate_report.py
@@ -1,19 +1,16 @@
 # Standard Libraries
 import re
 import json
-from os import system
 from datetime import date
 
 # Third Party
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
-
+from kubernetes import client, config
 
 JSON_FILE = "resources/table.json"
 REPORT_FILE = "docs/report.md"
-TEKTON_CI_OUT_FILE = "utils/tekton-ci-output.txt"
-TEKTON_CI_OUT_SCRIPT = "scripts/get-tekton-ci-output.sh"
 PIP_INDEX_URL = "https://pypi.org/pypi"
 
 SPEC_MAP = {
@@ -55,21 +52,16 @@ def get_upstream_version(dependency):
         return latest_version
 
 
-## Get the tekton ci output of the installed python dependencies
-system("bash " + TEKTON_CI_OUT_SCRIPT)
-
-with open(TEKTON_CI_OUT_FILE) as file:
-    content = file.read()
-
-
-def get_last_supported_version(dependency):
+def get_last_supported_version(tekton_ci_output, dependency):
     """get up-to-date supported version"""
     pattern = r"-([^\s]+)"
 
     if dependency == "Psycopg2":
         dependency = "psycopg2-binary"
 
-    last_supported_version = re.search(dependency + pattern, content, flags=re.I | re.M)
+    last_supported_version = re.search(
+        dependency + pattern, tekton_ci_output, flags=re.I | re.M
+    )
 
     return last_supported_version[1]
 
@@ -83,56 +75,100 @@ def isUptodate(last_supported_version, latest_version):
     return up_to_date
 
 
-# Read the JSON file
-with open(JSON_FILE) as file:
-    data = json.load(file)
+def get_tekton_ci_output():
+    # config.load_kube_config()
+    config.load_incluster_config()
+
+    group = "tekton.dev"
+    version = "v1"
+    namespace = "default"
+    plural = "taskruns"
+
+    # access the custom resource from tekton
+    tektonV1 = client.CustomObjectsApi()
+    taskruns = tektonV1.list_namespaced_custom_object(
+        group,
+        version,
+        namespace,
+        plural,
+        label_selector=f"{group}/task=python-tracer-unittest-default-task",
+    )["items"]
+
+    taskruns.sort(key=lambda tr: tr["metadata"]["creationTimestamp"], reverse=True)
+
+    coreV1 = client.CoreV1Api()
+    tekton_ci_output = ""
+    for tr in taskruns:
+        if (
+            re.match("python-trace\w+-unittest-default-3", tr["metadata"]["name"])
+            and tr["status"]["conditions"][0]["type"] == "Succeeded"
+        ):
+            pod = tr["status"]["podName"]
+            logs = coreV1.read_namespaced_pod_log(
+                pod, namespace, container="step-unittest"
+            )
+            if "Successfully installed" in logs:
+                for line in logs.splitlines():
+                    if "Successfully installed" in line:
+                        tekton_ci_output += line
+                break
+    return tekton_ci_output
 
 
-items = data["table"]
+def main():
+    # Read the JSON file
+    with open(JSON_FILE) as file:
+        data = json.load(file)
 
-for index in range(len(items)):
-    item = items[index]
-    package = item["package_name"]
+    items = data["table"]
+    tekton_ci_output = get_tekton_ci_output()
 
-    if "last_supported_version" not in item:
-        last_supported_version = get_last_supported_version(package)
-        item.update({"last_supported_version": last_supported_version})
-    else:
-        last_supported_version = item["last_supported_version"]
+    for item in items:
+        package = item["Package name"]
 
-    latest_version = get_upstream_version(package)
-    item.update({"latest_version": latest_version})
+        if "Last Supported Version" not in item:
+            last_supported_version = get_last_supported_version(
+                tekton_ci_output, package
+            )
+            item.update({"Last Supported Version": last_supported_version})
+        else:
+            last_supported_version = item["Last Supported Version"]
 
-    up_to_date = isUptodate(last_supported_version, latest_version)
+        latest_version = get_upstream_version(package)
 
-    item.update({"up_to_date": up_to_date})
+        up_to_date = isUptodate(last_supported_version, latest_version)
+
+        item.update({"Latest version": latest_version, "Up-to-date": up_to_date})
+
+    # Create a DataFrame from the list of dictionaries
+    df = pd.DataFrame(items)
+    df.insert(len(df.columns) - 1, "Cloud Native", df.pop("Cloud Native"))
+
+    # Rename Columns
+    df.columns = [
+        "Package name",
+        "Support Policy",
+        "Beta version",
+        "Last Supported Version",
+        "Latest version",
+        "Up-to-date",
+        "Cloud Native",
+    ]
+
+    # Convert dataframe to markdown
+    markdown_table = df.to_markdown(index=False)
+
+    current_date = date.today().strftime("%b %d, %Y")
+
+    disclaimer = f"##### This page is auto-generated. Any change will be overwritten after the next sync. Please apply changes directly to the files in the [python tracer](https://github.com/instana/python-sensor) repo. Last updated on **{current_date}**."
+    title = "## Python supported packages and versions"
+
+    # Combine disclaimer, title, and markdown table with line breaks
+    final_markdown = disclaimer + "\n" + title + "\n" + markdown_table
+
+    with open(REPORT_FILE, "w") as file:
+        file.write(final_markdown)
 
 
-# Create a DataFrame from the list of dictionaries
-df = pd.DataFrame(items)
-df.insert(len(df.columns) - 1, "cloud_native", df.pop("cloud_native"))
-
-# Rename Columns
-df.columns = [
-    "Package name",
-    "Support Policy",
-    "Beta version",
-    "Last Supported Version",
-    "Latest version",
-    "Up-to-date",
-    "Cloud Native",
-]
-
-# Convert dataframe to markdown
-markdown_table = df.to_markdown(index=False)
-
-current_date = date.today().strftime("%b %d, %Y")
-
-disclaimer = f"<sup>This page is auto-generated. Any change will be overwritten after the next sync. Please apply changes directly to the files in the [python tracer](https://github.com/instana/python-sensor) repo. Last updated on **{current_date}**.</sup>"
-title = "## Python supported packages and versions"
-
-# Combine disclaimer, title, and markdown table with line breaks
-final_markdown = disclaimer + "\n" + title + "\n" + markdown_table
-
-with open(REPORT_FILE, "w") as file:
-    file.write(final_markdown)
+if __name__ == "__main__":
+    main()

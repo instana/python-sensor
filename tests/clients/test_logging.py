e1041da4b7c0e18e49140fdcf816a5df666079ba
Ferenc Géczi <ferenc.geczi@ibm.com>
2024-05-23 12:00:00 +0000

    feat: Add opt-in root exit spans to logging
    Signed-off-by: Ferenc Géczi <ferenc.geczi@ibm.com>

--- tests/clients/test_logging.py
+++ tests/clients/test_logging.py
@@ -3,7 +3,7 @@
 
 import logging
 import unittest
-from instana.singletons import tracer
+from instana.singletons import agent, tracer
 
 
 class TestLogging(unittest.TestCase):
@@ -14,8 +14,8 @@ class TestLogging(unittest.TestCase):
         self.logger = logging.getLogger('unit test')
 
     def tearDown(self):
-        """ Do nothing for now """
-        return None
+        """ Ensure that allow_exit_as_root has the default value """
+        agent.options.allow_exit_as_root = False
 
     def test_no_span(self):
         with tracer.start_active_span('test'):
@@ -58,3 +58,19 @@ class TestLogging(unittest.TestCase):
 
         self.assertIsNotNone(spans[0].data["log"].get('parameters'))
 
+    def test_no_root_exit_span(self):
+        agent.options.allow_exit_as_root = True
+        self.logger.info('info message')
+
+        spans = self.recorder.queued_spans()
+        self.assertEqual(0, len(spans))
+
+    def test_root_exit_span(self):
+        agent.options.allow_exit_as_root = True
+        self.logger.warning('foo %s', 'bar')
+
+        spans = self.recorder.queued_spans()
+        self.assertEqual(1, len(spans))
+        self.assertEqual(2, spans[0].k)
+
+        self.assertEqual('foo bar', spans[0].data["log"].get('message'))

f262292a57aea48aa10d8bf8534a93b0d2f62fc7
Paulo Vital <paulo.vital@ibm.com>
2024-05-27 16:37:27 +0200

    style: add type hints in the traceutils.py file.
    Used ruff (vscode) to:
    - isort-compatible import sorting.
    
    Signed-off-by: Paulo Vital <paulo.vital@ibm.com>

--- src/instana/util/traceutils.py
+++ src/instana/util/traceutils.py
@@ -1,11 +1,15 @@
 # (c) Copyright IBM Corp. 2021
 # (c) Copyright Instana Inc. 2021
 
-from ..singletons import agent, tracer, async_tracer, tornado_tracer
-from ..log import logger
+from typing import Optional, Tuple
+
+from instana.log import logger
+from instana.singletons import agent, async_tracer, tornado_tracer, tracer
+from instana.span import InstanaSpan
+from instana.tracer import InstanaTracer
 
 
-def extract_custom_headers(tracing_span, headers):
+def extract_custom_headers(tracing_span, headers) -> None:
     try:
         for custom_header in agent.options.extra_http_headers:
             # Headers are in the following format: b'x-header-1'
@@ -16,7 +20,7 @@ def extract_custom_headers(tracing_span, headers):
         logger.debug("extract_custom_headers: ", exc_info=True)
 
 
-def get_active_tracer():
+def get_active_tracer() -> Optional[InstanaTracer]:
     try:
         if tracer.active_span:
             return tracer
@@ -32,7 +36,7 @@ def get_active_tracer():
         return None
 
 
-def get_tracer_tuple():
+def get_tracer_tuple() -> Tuple[Optional[InstanaTracer], Optional[InstanaSpan], Optional[str]]:
     active_tracer = get_active_tracer()
     if active_tracer:
         return (active_tracer, active_tracer.active_span, active_tracer.active_span.operation_name)
@@ -41,5 +45,5 @@ def get_tracer_tuple():
     return (None, None, None)
 
 
-def tracing_is_off():
+def tracing_is_off() -> bool:
     return not (bool(get_active_tracer()) or agent.options.allow_exit_as_root)

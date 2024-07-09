f9479b2ed5391871ff0bf88dd3f75c1fc5052f58
Varsha GS <varsha.gs@ibm.com>
2024-07-09 13:23:52 +0530

    instrumentation(flask): Adapt common to OTel spec Signed-off-by: Varsha GS <varsha.gs@ibm.com>

--- src/instana/instrumentation/flask/__init__.py
+++ src/instana/instrumentation/flask/__init__.py
@@ -11,13 +11,13 @@ try:
     # Blinker support is preferred but we do the best we can when it's not available.
     #
     if hasattr(flask.signals, 'signals_available'):
-      from flask.signals import signals_available
+        from flask.signals import signals_available
     else:
-      # Beginning from 2.3.0 as stated in the notes
-      # https://flask.palletsprojects.com/en/2.3.x/changes/#version-2-3-0
-      # "Signals are always available. blinker>=1.6.2 is a required dependency.
-      # The signals_available attribute is deprecated. #5056"
-      signals_available = True
+        # Beginning from 2.3.0 as stated in the notes
+        # https://flask.palletsprojects.com/en/2.3.x/changes/#version-2-3-0
+        # "Signals are always available. blinker>=1.6.2 is a required dependency.
+        # The signals_available attribute is deprecated. #5056"
+        signals_available = True
 
     from . import common
 

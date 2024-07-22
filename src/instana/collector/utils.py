51337c2525e09ae0be26bf2256695b562ce63ebc
Varsha GS <varsha.gs@ibm.com>
2024-07-22 13:30:33 +0530

    fix: Report data to Agent
    Signed-off-by: Varsha GS <varsha.gs@ibm.com>

--- src/instana/collector/utils.py
+++ src/instana/collector/utils.py
@@ -3,16 +3,17 @@
 from typing import TYPE_CHECKING, List
 
 from opentelemetry.trace.span import format_span_id
+from opentelemetry.trace import SpanKind
 
 if TYPE_CHECKING:
     from instana.span.span import InstanaSpan
 
 
-def format_trace_and_span_ids(
+def format_span(
     queued_spans: List["InstanaSpan"],
 ) -> List["InstanaSpan"]:
     """
-    Format the Trace, Parent Span, and Span IDs of Spans to be a 64-bit
+    Format Span Kind and the Trace, Parent Span and Span IDs of the Spans to be a 64-bit
     Hexadecimal String instead of Integer before being pushed to a
     Collector (or Instana Agent).
     """
@@ -21,5 +22,7 @@ def format_trace_and_span_ids(
         span.t = format_span_id(span.t)
         span.s = format_span_id(span.s)
         span.p = format_span_id(span.p) if span.p else None
+        if isinstance(span.k, SpanKind):
+            span.k = span.k.value if not span.k is SpanKind.INTERNAL else 3
         spans.append(span)
     return spans

db1ba5220df7730abf0a06b368f5dc521bd50930
Cagri Yonca <cagriyonca@gmail.com>
2024-08-07 16:39:15 +0200

    implementation: added opentelemetry support and related unittests
    Signed-off-by: Cagri Yonca <cagriyonca@gmail.com>

--- src/instana/instrumentation/pep0249.py
+++ src/instana/instrumentation/pep0249.py
@@ -2,105 +2,125 @@
 # (c) Copyright Instana Inc. 2018
 
 # This is a wrapper for PEP-0249: Python Database API Specification v2.0
-import opentracing.ext.tags as ext
+from __future__ import annotations
 import wrapt
+from typing import TYPE_CHECKING
 
-from ..log import logger
-from ..util.traceutils import get_tracer_tuple, tracing_is_off
-from ..util.sql import sql_sanitizer
+from opentelemetry.semconv.trace import SpanAttributes
+from opentelemetry.trace import SpanKind
+
+from instana.log import logger
+from instana.util.traceutils import get_tracer_tuple, tracing_is_off
+from instana.util.sql import sql_sanitizer
+
+if TYPE_CHECKING:
+    from instana.span.span import InstanaSpan
 
 
 class CursorWrapper(wrapt.ObjectProxy):
-    __slots__ = ('_module_name', '_connect_params', '_cursor_params')
+    __slots__ = ("_module_name", "_connect_params", "_cursor_params")
 
-    def __init__(self, cursor, module_name,
-                 connect_params=None, cursor_params=None):
+    def __init__(
+        self, cursor, module_name, connect_params=None, cursor_params=None
+    ) -> None:
         super(CursorWrapper, self).__init__(wrapped=cursor)
         self._module_name = module_name
         self._connect_params = connect_params
         self._cursor_params = cursor_params
 
-    def _collect_kvs(self, span, sql):
+    def _collect_kvs(self, span, sql) -> InstanaSpan:
         try:
-            span.set_tag(ext.SPAN_KIND, 'exit')
+            span.set_attribute(SpanKind, "exit")
 
-            db_parameter_name = next((p for p in ('db', 'database', 'dbname') if p in self._connect_params[1]), None)
+            db_parameter_name = next(
+                (
+                    p
+                    for p in ("db", "database", "dbname")
+                    if p in self._connect_params[1]
+                ),
+                None,
+            )
             if db_parameter_name:
-                span.set_tag(ext.DATABASE_INSTANCE, self._connect_params[1][db_parameter_name])
+                span.set_attribute(
+                    SpanAttributes.DB_NAME,
+                    self._connect_params[1][db_parameter_name],
+                )
 
-            span.set_tag(ext.DATABASE_STATEMENT, sql_sanitizer(sql))
-            span.set_tag(ext.DATABASE_USER, self._connect_params[1]['user'])
-            span.set_tag('host', self._connect_params[1]['host'])
-            span.set_tag('port', self._connect_params[1]['port'])
+            span.set_attribute(SpanAttributes.DB_STATEMENT, sql_sanitizer(sql))
+            span.set_attribute(SpanAttributes.DB_USER, self._connect_params[1]["user"])
+            span.set_attribute("host", self._connect_params[1]["host"])
+            span.set_attribute("port", self._connect_params[1]["port"])
         except Exception as e:
             logger.debug(e)
         return span
 
-    def __enter__(self):
+    def __enter__(self) -> CursorWrapper:
         return self
 
-    def execute(self, sql, params=None):
-        tracer, parent_span, operation_name = get_tracer_tuple()
+    def execute(self, sql, params=None) -> None:
+        tracer, _, operation_name = get_tracer_tuple()
 
         # If not tracing or we're being called from sqlalchemy, just pass through
-        if (tracing_is_off() or (operation_name == "sqlalchemy")):
+        if tracing_is_off() or (operation_name == "sqlalchemy"):
             return self.__wrapped__.execute(sql, params)
 
-        with tracer.start_active_span(self._module_name, child_of=parent_span) as scope:
+        with tracer.start_as_current_span(self._module_name) as span:
             try:
-                self._collect_kvs(scope.span, sql)
-
+                self._collect_kvs(span, sql)
                 result = self.__wrapped__.execute(sql, params)
             except Exception as e:
-                if scope.span:
-                    scope.span.log_exception(e)
+                if span:
+                    span.record_exception(e)
                 raise
             else:
                 return result
 
-    def executemany(self, sql, seq_of_parameters):
-        tracer, parent_span, operation_name = get_tracer_tuple()
+    def executemany(self, sql, seq_of_parameters) -> None:
+        tracer, _, operation_name = get_tracer_tuple()
 
         # If not tracing or we're being called from sqlalchemy, just pass through
-        if (tracing_is_off() or (operation_name == "sqlalchemy")):
+        if tracing_is_off() or (operation_name == "sqlalchemy"):
             return self.__wrapped__.executemany(sql, seq_of_parameters)
 
-        with tracer.start_active_span(self._module_name, child_of=parent_span) as scope:
+        with tracer.start_as_current_span(self._module_name) as span:
             try:
-                self._collect_kvs(scope.span, sql)
-
+                self._collect_kvs(span, sql)
                 result = self.__wrapped__.executemany(sql, seq_of_parameters)
             except Exception as e:
-                if scope.span:
-                    scope.span.log_exception(e)
+                if span:
+                    span.record_exception(e)
                 raise
             else:
                 return result
 
-    def callproc(self, proc_name, params):
-        tracer, parent_span, operation_name = get_tracer_tuple()
+    def callproc(self, proc_name, params) -> None:
+        tracer, _, operation_name = get_tracer_tuple()
 
         # If not tracing or we're being called from sqlalchemy, just pass through
-        if (tracing_is_off() or (operation_name == "sqlalchemy")):
+        if tracing_is_off() or (operation_name == "sqlalchemy"):
             return self.__wrapped__.execute(proc_name, params)
 
-        with tracer.start_active_span(self._module_name, child_of=parent_span) as scope:
+        with tracer.start_as_current_span(self._module_name) as span:
             try:
-                self._collect_kvs(scope.span, proc_name)
-
+                self._collect_kvs(span, proc_name)
                 result = self.__wrapped__.callproc(proc_name, params)
-            except Exception as e:
-                if scope.span:
-                    scope.span.log_exception(e)
-                raise
+            except Exception:
+                try:
+                    result = self.__wrapped__.execute(proc_name, params)
+                except Exception as e_execute:
+                    if span:
+                        span.record_exception(e_execute)
+                    raise
+                else:
+                    return result
             else:
                 return result
 
 
 class ConnectionWrapper(wrapt.ObjectProxy):
-    __slots__ = ('_module_name', '_connect_params')
+    __slots__ = ("_module_name", "_connect_params")
 
-    def __init__(self, connection, module_name, connect_params):
+    def __init__(self, connection, module_name, connect_params) -> None:
         super(ConnectionWrapper, self).__init__(wrapped=connection)
         self._module_name = module_name
         self._connect_params = connect_params
@@ -108,33 +128,35 @@ class ConnectionWrapper(wrapt.ObjectProxy):
     def __enter__(self):
         return self
 
-    def cursor(self, *args, **kwargs):
+    def cursor(self, *args, **kwargs) -> CursorWrapper:
         return CursorWrapper(
             cursor=self.__wrapped__.cursor(*args, **kwargs),
             module_name=self._module_name,
             connect_params=self._connect_params,
-            cursor_params=(args, kwargs) if args or kwargs else None)
+            cursor_params=(args, kwargs) if args or kwargs else None,
+        )
 
-    def begin(self):
-        return self.__wrapped__.begin()
+    def close(self) -> None:
+        return self.__wrapped__.close()
 
-    def commit(self):
+    def commit(self) -> None:
         return self.__wrapped__.commit()
 
-    def rollback(self):
+    def rollback(self) -> None:
         return self.__wrapped__.rollback()
 
 
 class ConnectionFactory(object):
-    def __init__(self, connect_func, module_name):
+    def __init__(self, connect_func, module_name) -> None:
         self._connect_func = connect_func
         self._module_name = module_name
         self._wrapper_ctor = ConnectionWrapper
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, *args, **kwargs) -> ConnectionWrapper:
         connect_params = (args, kwargs) if args or kwargs else None
 
         return self._wrapper_ctor(
             connection=self._connect_func(*args, **kwargs),
             module_name=self._module_name,
-            connect_params=connect_params)
+            connect_params=connect_params,
+        )
---
+++ tests/clients/test_pep0249.py
@@ -0,0 +1,319 @@
+import logging
+from typing import Generator, TYPE_CHECKING
+from unittest.mock import patch
+import psycopg2
+import psycopg2.extras
+import pytest
+from instana.instrumentation.pep0249 import (
+    ConnectionFactory,
+    ConnectionWrapper,
+    CursorWrapper,
+)
+from opentelemetry.trace import SpanKind
+from instana.singletons import tracer
+from instana.util.traceutils import get_tracer_tuple
+from pytest import LogCaptureFixture
+from instana.span.span import InstanaSpan
+
+
+class TestCursorWrapper:
+    @pytest.fixture(autouse=True)
+    def _setup(self) -> Generator[None, None, None]:
+        self.connect_params = [
+            "db",
+            {
+                "db": "instana_test_db",
+                "host": "localhost",
+                "port": "5432",
+                "user": "root",
+                "password": "passw0rd",
+            },
+        ]
+        self.test_conn = psycopg2.connect(
+            database=self.connect_params[1]["db"],
+            host=self.connect_params[1]["host"],
+            port=self.connect_params[1]["port"],
+            user=self.connect_params[1]["user"],
+            password=self.connect_params[1]["password"],
+        )
+        self.cursor_params = {"key": "value"}
+        self.test_cursor = self.test_conn.cursor()
+        self.cursor_name = "test-cursor"
+        self.test_wrapper = CursorWrapper(
+            self.test_cursor,
+            self.cursor_name,
+            self.connect_params,
+            self.cursor_params,
+        )
+        self.test_cursor.execute(
+            """
+            DROP TABLE IF EXISTS tests;
+            CREATE TABLE tests (id SERIAL PRIMARY KEY, name VARCHAR(50), email VARCHAR(100));
+            """
+        )
+        self.test_conn.commit()
+        self.test_cursor.execute(
+            """
+            INSERT INTO tests (id, name, email) VALUES (1, 'test-name', 'testemail@mail.com');
+            """
+        )
+        self.test_conn.commit()
+        self.test_cursor.execute("""
+            CREATE OR REPLACE PROCEDURE insert_user(IN test_id INT, IN test_name VARCHAR, IN test_email VARCHAR)
+            LANGUAGE plpgsql
+            AS $$
+            BEGIN
+                INSERT INTO tests (id, name, email) VALUES (test_id, test_name, test_email);
+            END;
+            $$;
+            """)
+        self.test_conn.commit()
+        yield
+        self.test_cursor.close()
+        self.test_conn.close()
+
+    def test_cursor_wrapper_default(self):
+        # CursorWrapper
+        assert self.test_wrapper
+        assert self.test_wrapper._module_name == self.cursor_name
+        connection_params = {"db", "host", "port", "user", "password"}
+        assert connection_params.issubset(self.test_wrapper._connect_params[1].keys())
+        assert not self.test_wrapper.closed
+        assert self.test_wrapper._cursor_params == self.cursor_params
+
+        # Test Connection
+        assert (
+            self.test_conn.dsn
+            == "user=root password=xxx dbname=instana_test_db host=localhost port=5432"
+        )
+        assert not self.test_conn.autocommit
+        assert self.test_conn.status == 1
+        assert self.test_conn.info.dbname == "instana_test_db"
+        assert self.test_conn.info.host == "localhost"
+        assert self.test_conn.info.user == "root"
+        assert self.test_conn.info.port == 5432
+
+        # Test Cursor
+        assert self.test_cursor.arraysize == 1
+        assert isinstance(self.test_cursor, psycopg2.extensions.cursor)
+        assert hasattr(self.test_cursor, "callproc")
+        assert hasattr(self.test_cursor, "close")
+        assert hasattr(self.test_cursor, "execute")
+        assert hasattr(self.test_cursor, "executemany")
+        assert hasattr(self.test_cursor, "fetchone")
+        assert hasattr(self.test_cursor, "fetchall")
+
+    def test_collect_kvs(self):
+        with tracer.start_as_current_span("test") as span:
+            sample_sql = """
+                select * from tests;
+            """
+            self.test_wrapper._collect_kvs(span, sample_sql)
+            assert span.attributes[SpanKind] == "exit"
+            assert span.attributes["db.name"] == "instana_test_db"
+            assert span.attributes["db.statement"] == sample_sql
+            assert span.attributes["db.user"] == "root"
+            assert span.attributes["host"] == "localhost"
+            assert span.attributes["port"] == "5432"
+
+    def test_collect_kvs_error(self, caplog: LogCaptureFixture):
+        with tracer.start_as_current_span("test") as span:
+            connect_params = "sample"
+            sample_wrapper = CursorWrapper(
+                self.test_cursor,
+                self.cursor_name,
+                connect_params,
+            )
+            sample_sql = "select * from tests;"
+            caplog.set_level(logging.DEBUG, logger="instana")
+            sample_wrapper._collect_kvs(span, sample_sql)
+            assert "string indices must be integers" in caplog.messages
+
+    def test_enter(self):
+        response = self.test_wrapper.__enter__()
+        assert response == self.test_wrapper
+        assert isinstance(response, CursorWrapper)
+
+    def test_execute_with_tracing_off(self):
+        with tracer.start_as_current_span("sqlalchemy"):
+            sample_sql = """insert into tests (id, name, email) values (%s, %s, %s) returning id, name, email;"""
+            sample_params = (2, "sample-name", "sample-email@mail.com")
+            with patch(
+                "instana.instrumentation.pep0249.get_tracer_tuple",
+                wraps=get_tracer_tuple,
+            ) as mock_get_tracer_tuple:
+                self.test_wrapper.execute(sample_sql, sample_params)
+                mock_get_tracer_tuple.assert_called_once()
+            self.test_wrapper.execute("select * from tests;")
+            response = self.test_wrapper.fetchall()
+            assert sample_params in response
+            assert len(response) == 2
+
+    def test_execute_with_tracing(self):
+        with tracer.start_as_current_span("test"):
+            sample_sql = """insert into tests (id, name, email) values (%s, %s, %s) returning id, name, email;"""
+            sample_params = (3, "sample-name", "sample-email@mail.com")
+            self.test_wrapper.execute(sample_sql, sample_params)
+            last_inserted_row = self.test_cursor.fetchone()
+            self.test_conn.commit()
+            assert last_inserted_row == sample_params
+
+            # Exception Handling
+            with pytest.raises(Exception) as exc_info, patch.object(
+                CursorWrapper, "_collect_kvs", side_effect=Exception("test exception")
+            ) as mock_collect_kvs:
+                self.test_wrapper.execute(sample_sql)
+            assert str(exc_info.value) == "test exception"
+            mock_collect_kvs.assert_called_once()
+            self.test_wrapper.execute("select * from tests;")
+            response = self.test_wrapper.fetchall()
+            assert sample_params in response
+            assert len(response) == 2
+
+    def test_executemany_with_tracing_off(self):
+        with tracer.start_as_current_span("sqlalchemy"):
+            sample_sql = """insert into tests (id, name, email) values (%s, %s, %s) returning id, name, email;"""
+            sample_seq_of_params = [
+                (4, "sample-name-3", "sample-email-3@mail.com"),
+                (5, "sample-name-4", "sample-email-4@mail.com"),
+            ]
+            with patch(
+                "instana.instrumentation.pep0249.get_tracer_tuple",
+                wraps=get_tracer_tuple,
+            ) as mocked_object:
+                self.test_wrapper.executemany(sample_sql, sample_seq_of_params)
+                mocked_object.assert_called_once()
+            self.test_wrapper.execute("select * from tests;")
+            response = self.test_wrapper.fetchall()
+            for record in sample_seq_of_params:
+                assert record in response
+            assert len(response) == 3
+
+    def test_executemany_with_tracing(self):
+        with tracer.start_as_current_span("test"):
+            sample_sql = """insert into tests (id, name, email) values (%s, %s, %s) returning id, name, email;"""
+            sample_seq_of_params = [
+                (6, "sample-name-3", "sample-email-3@mail.com"),
+                (7, "sample-name-4", "sample-email-4@mail.com"),
+            ]
+            self.test_wrapper.executemany(sample_sql, sample_seq_of_params)
+
+            # Exception Handling
+            with pytest.raises(Exception) as exc_info, patch.object(
+                CursorWrapper, "_collect_kvs", side_effect=Exception("test exception")
+            ) as mock_collect_kvs:
+                self.test_wrapper.executemany(
+                    sample_sql, seq_of_parameters=sample_seq_of_params
+                )
+            assert str(exc_info.value) == "test exception"
+            mock_collect_kvs.assert_called_once()
+            self.test_wrapper.execute("select * from tests;")
+            response = self.test_wrapper.fetchall()
+            for record in sample_seq_of_params:
+                assert record in response
+            assert len(response) == 3
+
+    def test_callproc_with_tracing_off(self):
+        with tracer.start_as_current_span("sqlalchemy"):
+            sample_proc_name = "call insert_user(%s, %s, %s);"
+            sample_params = (8, "sample-name-8", "sample-email-8@mail.com")
+            self.test_wrapper.callproc(sample_proc_name, sample_params)
+            self.test_conn.commit()
+            self.test_wrapper.execute("select * from tests;")
+            response = self.test_wrapper.fetchall()
+            assert sample_params in response
+            assert len(response) == 2
+
+    def test_callproc_with_tracing(self):
+        with tracer.start_as_current_span("test"):
+            sample_proc_name = "call insert_user(%s, %s, %s);"
+            sample_params = (9, "sample-name-9", "sample-email-9@mail.com")
+            self.test_wrapper.callproc(sample_proc_name, sample_params)
+            self.test_conn.commit()
+            self.test_wrapper.execute("select * from tests;")
+            response = self.test_wrapper.fetchall()
+            assert sample_params in response
+            assert len(response) == 2
+
+            # Exception Handling
+            error_proc_name = "erroroeus command;"
+            with pytest.raises(Exception) as exc_info, patch.object(
+                InstanaSpan,
+                "record_exception",
+            ) as mock_exception:
+                self.test_wrapper.callproc(error_proc_name, sample_params)
+            assert exc_info.typename == "SyntaxError"
+            mock_exception.call_count == 2
+
+
+class TestConnectionWrapper:
+    @pytest.fixture(autouse=True)
+    def _setup(self) -> Generator[None, None, None]:
+        self.connect_params = [
+            "db",
+            {
+                "db": "instana_test_db",
+                "host": "localhost",
+                "port": "5432",
+                "user": "root",
+                "password": "passw0rd",
+            },
+        ]
+        self.test_conn = psycopg2.connect(
+            database=self.connect_params[1]["db"],
+            host=self.connect_params[1]["host"],
+            port=self.connect_params[1]["port"],
+            user=self.connect_params[1]["user"],
+            password=self.connect_params[1]["password"],
+        )
+        self.module_name = "test-connection"
+        self.connection_manager = ConnectionWrapper(
+            self.test_conn, self.module_name, self.connect_params
+        )
+        yield
+        self.test_conn.close()
+
+    def test_enter(self):
+        response = self.connection_manager.__enter__()
+        assert isinstance(response, ConnectionWrapper)
+        assert response._module_name == self.module_name
+        assert response._connect_params == self.connect_params
+
+    def test_cursor(self):
+        response = self.connection_manager.cursor()
+        assert isinstance(response, CursorWrapper)
+
+    def test_close(self):
+        response = self.connection_manager.close()
+        assert self.test_conn.closed
+        assert not response
+
+    def test_commit(self):
+        response = self.connection_manager.commit()
+        assert not response
+
+    def test_rollback(self):
+        if hasattr(self.connection_manager, "rollback"):
+            response = self.connection_manager.rollback()
+            assert not response
+
+
+class TestConnectionFactory:
+    @pytest.fixture(autouse=True)
+    def _setup(self) -> Generator[None, None, None]:
+        self.test_conn_func = psycopg2.extras.LogicalReplicationConnection
+        self.test_module_name = "test-factory"
+        self.conn_fact = ConnectionFactory(self.test_conn_func, self.test_module_name)
+        yield
+        self.test_conn_func = None
+        self.test_module_name = None
+        self.conn_fact = None
+
+    def test_call(self):
+        response = self.conn_fact(
+            dsn="user=root password=passw0rd dbname=instana_test_db host=localhost port=5432"
+        )
+        assert isinstance(self.conn_fact._wrapper_ctor, ConnectionWrapper.__class__)
+        assert self.conn_fact._connect_func == self.test_conn_func
+        assert self.conn_fact._module_name == self.test_module_name
+        assert isinstance(response, ConnectionWrapper)

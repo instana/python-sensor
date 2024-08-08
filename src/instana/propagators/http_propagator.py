3f14cee0bd909fd7147e8aba1f7726473f1561b3
Ferenc Géczi <ferenc.geczi@ibm.com>
2024-08-08 12:00:00 +0000

    fix: Ensure extraction of headers from dictionary
    Signed-off-by: Ferenc Géczi <ferenc.geczi@ibm.com>

--- src/instana/propagators/http_propagator.py
+++ src/instana/propagators/http_propagator.py
@@ -20,10 +20,13 @@ class HTTPPropagator(BasePropagator):
     def inject(self, span_context, carrier, disable_w3c_trace_context=False):
         trace_id = span_context.trace_id
         span_id = span_context.span_id
-        # Suppression `level` made in the child context or in the parent context
-        # has priority over any non-suppressed `level` setting
-        child_level = int(self.extract_instana_headers(carrier)[2] or "1")
-        span_context.level = min(child_level, span_context.level)
+        dictionary_carrier = self.extract_headers_dict(carrier)
+        if dictionary_carrier:
+            # Suppression `level` made in the child context or in the parent context
+            # has priority over any non-suppressed `level` setting
+            child_level = int(self.extract_instana_headers(dictionary_carrier)[2] or "1")
+            span_context.level = min(child_level, span_context.level)
+
         serializable_level = str(span_context.level)
 
         if disable_w3c_trace_context:

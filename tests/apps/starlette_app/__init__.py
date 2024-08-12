81b7bc9d8ce1b3781b294ec7d33cbf607985349c
Paulo Vital <paulo.vital@ibm.com>
2024-08-12 16:58:33 +0200

    tests(asgi+starlette): adapt tests to OTel usage.
    Signed-off-by: Paulo Vital <paulo.vital@ibm.com>

--- tests/apps/starlette_app/__init__.py
+++ tests/apps/starlette_app/__init__.py
@@ -2,17 +2,28 @@
 # (c) Copyright Instana Inc. 2020
 
 import uvicorn
-from ...helpers import testenv
-from instana.log import logger
 
+from tests.helpers import testenv
+
+testenv["starlette_host"] = "127.0.0.1"
 testenv["starlette_port"] = 10817
-testenv["starlette_server"] = ("http://127.0.0.1:" + str(testenv["starlette_port"]))
+testenv["starlette_server"] = "http://" + testenv["starlette_host"] + ":" + str(testenv["starlette_port"])
+
+
 
 def launch_starlette():
     from .app import starlette_server
     from instana.singletons import agent
 
     # Hack together a manual custom headers list; We'll use this in tests
-    agent.options.extra_http_headers = [u'X-Capture-This', u'X-Capture-That']
+    agent.options.extra_http_headers = [
+        "X-Capture-This", 
+        "X-Capture-That",
+    ]
 
-    uvicorn.run(starlette_server, host='127.0.0.1', port=testenv['starlette_port'], log_level="critical")
+    uvicorn.run(
+        starlette_server,
+        host=testenv["starlette_host"],
+        port=testenv["starlette_port"],
+        log_level="critical",
+    )

329fcece6ae4fb9f0617283609200b459f457779
Paulo Vital <paulo.vital@ibm.com>
2024-08-12 16:57:54 +0200

    refactor: Starlette instrumentation.
    Signed-off-by: Paulo Vital <paulo.vital@ibm.com>

--- src/instana/instrumentation/starlette_inst.py
+++ src/instana/instrumentation/starlette_inst.py
@@ -5,18 +5,29 @@
 Instrumentation for Starlette
 https://www.starlette.io/
 """
+
+from typing import Any, Callable, Dict, Tuple
+
+import starlette.applications
+
 try:
     import starlette
     import wrapt
-    from ..log import logger
-    from .asgi import InstanaASGIMiddleware
     from starlette.middleware import Middleware
 
-    @wrapt.patch_function_wrapper('starlette.applications', 'Starlette.__init__')
-    def init_with_instana(wrapped, instance, args, kwargs):
-        middleware = kwargs.get('middleware')
+    from instana.instrumentation.asgi import InstanaASGIMiddleware
+    from instana.log import logger
+
+    @wrapt.patch_function_wrapper("starlette.applications", "Starlette.__init__")
+    def init_with_instana(
+        wrapped: Callable[..., starlette.applications.Starlette.__init__],
+        instance: starlette.applications.Starlette,
+        args: Tuple,
+        kwargs: Dict[str, Any],
+    ) -> None:
+        middleware = kwargs.get("middleware")
         if middleware is None:
-            kwargs['middleware'] = [Middleware(InstanaASGIMiddleware)]
+            kwargs["middleware"] = [Middleware(InstanaASGIMiddleware)]
         elif isinstance(middleware, list):
             middleware.append(Middleware(InstanaASGIMiddleware))
 

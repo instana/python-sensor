1b8eab1c479f0cecfdb55cf21a35e90522ae4125
Cagri Yonca <cagriyonca@gmail.com>
2024-09-03 12:59:06 +0300

    refactor(mysqlclient): added instrumentation of mysqlclient
    Signed-off-by: Cagri Yonca <cagriyonca@gmail.com>

--- src/instana/instrumentation/mysqlclient.py
+++ src/instana/instrumentation/mysqlclient.py
@@ -2,17 +2,17 @@
 # (c) Copyright Instana Inc. 2019
 
 
-from ..log import logger
-from .pep0249 import ConnectionFactory
+from instana.log import logger
+from instana.instrumentation.pep0249 import ConnectionFactory
 
 try:
     import MySQLdb
 
-    cf = ConnectionFactory(connect_func=MySQLdb.connect, module_name='mysql')
+    cf = ConnectionFactory(connect_func=MySQLdb.connect, module_name="mysql")
 
-    setattr(MySQLdb, 'connect', cf)
-    if hasattr(MySQLdb, 'Connect'):
-        setattr(MySQLdb, 'Connect', cf)
+    setattr(MySQLdb, "connect", cf)
+    if hasattr(MySQLdb, "Connect"):
+        setattr(MySQLdb, "Connect", cf)
 
     logger.debug("Instrumenting mysqlclient")
 except ImportError:

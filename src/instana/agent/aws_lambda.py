16f103678a5ccf97cd20e0d4654febad1a2d007d
Paulo Vital <paulo.vital@ibm.com>
2024-09-12 17:59:45 +0200

    refactor: AWS Lambda instrumentation.
    Signed-off-by: Paulo Vital <paulo.vital@ibm.com>

--- src/instana/agent/aws_lambda.py
+++ src/instana/agent/aws_lambda.py
@@ -2,21 +2,23 @@
 # (c) Copyright Instana Inc. 2020
 
 """
-The Instana agent (for AWS Lambda functions) that manages
+The Instana Agent for AWS Lambda functions that manages
 monitoring state and reporting that data.
 """
-import time
-from ..log import logger
-from ..util import to_json
-from .base import BaseAgent
-from ..version import VERSION
-from ..collector.aws_lambda import AWSLambdaCollector
-from ..options import AWSLambdaOptions
+
+from typing import Any, Dict
+from instana.agent.base import BaseAgent
+from instana.collector.aws_lambda import AWSLambdaCollector
+from instana.log import logger
+from instana.options import AWSLambdaOptions
+from instana.util import to_json
+from instana.version import VERSION
 
 
 class AWSLambdaAgent(BaseAgent):
-    """ In-process agent for AWS Lambda """
-    def __init__(self):
+    """In-process Agent for AWS Lambda"""
+
+    def __init__(self) -> None:
         super(AWSLambdaAgent, self).__init__()
 
         self.collector = None
@@ -27,29 +29,33 @@ class AWSLambdaAgent(BaseAgent):
         # Update log level from what Options detected
         self.update_log_level()
 
-        logger.info("Stan is on the AWS Lambda scene.  Starting Instana instrumentation version: %s", VERSION)
+        logger.info(
+            f"Stan is on the AWS Lambda scene.  Starting Instana instrumentation version: {VERSION}",
+        )
 
         if self._validate_options():
             self._can_send = True
             self.collector = AWSLambdaCollector(self)
             self.collector.start()
         else:
-            logger.warning("Required INSTANA_AGENT_KEY and/or INSTANA_ENDPOINT_URL environment variables not set.  "
-                           "We will not be able monitor this function.")
+            logger.warning(
+                "Required INSTANA_AGENT_KEY and/or INSTANA_ENDPOINT_URL environment variables not set.  "
+                "We will not be able monitor this function."
+            )
 
-    def can_send(self):
+    def can_send(self) -> bool:
         """
         Are we in a state where we can send data?
         @return: Boolean
         """
         return self._can_send
 
-    def get_from_structure(self):
+    def get_from_structure(self) -> Dict[str, Any]:
         """
         Retrieves the From data that is reported alongside monitoring data.
         @return: dict()
         """
-        return {'hl': True, 'cp': 'aws', 'e': self.collector.get_fq_arn()}
+        return {"hl": True, "cp": "aws", "e": self.collector.get_fq_arn()}
 
     def report_data_payload(self, payload):
         """
@@ -64,30 +70,38 @@ class AWSLambdaAgent(BaseAgent):
                 self.report_headers["X-Instana-Host"] = self.collector.get_fq_arn()
                 self.report_headers["X-Instana-Key"] = self.options.agent_key
 
-            response = self.client.post(self.__data_bundle_url(),
-                                        data=to_json(payload),
-                                        headers=self.report_headers,
-                                        timeout=self.options.timeout,
-                                        verify=self.options.ssl_verify,
-                                        proxies=self.options.endpoint_proxy)
+            response = self.client.post(
+                self.__data_bundle_url(),
+                data=to_json(payload),
+                headers=self.report_headers,
+                timeout=self.options.timeout,
+                verify=self.options.ssl_verify,
+                proxies=self.options.endpoint_proxy,
+            )
 
             if 200 <= response.status_code < 300:
-                logger.debug("report_data_payload: Instana responded with status code %s", response.status_code)
+                logger.debug(
+                    "report_data_payload: Instana responded with status code %s",
+                    response.status_code,
+                )
             else:
-                logger.info("report_data_payload: Instana responded with status code %s", response.status_code)
+                logger.info(
+                    "report_data_payload: Instana responded with status code %s",
+                    response.status_code,
+                )
         except Exception as exc:
             logger.debug("report_data_payload: connection error (%s)", type(exc))
 
         return response
 
-    def _validate_options(self):
+    def _validate_options(self) -> bool:
         """
         Validate that the options used by this Agent are valid.  e.g. can we report data?
         """
-        return self.options.endpoint_url is not None and self.options.agent_key is not None
+        return self.options.endpoint_url and self.options.agent_key
 
-    def __data_bundle_url(self):
+    def __data_bundle_url(self) -> str:
         """
         URL for posting metrics to the host agent.  Only valid when announced.
         """
-        return "%s/bundle" % self.options.endpoint_url
+        return f"{self.options.endpoint_url}/bundle"
--- src/instana/instrumentation/aws/lambda_inst.py
+++ src/instana/instrumentation/aws/lambda_inst.py
@@ -4,66 +4,89 @@
 """
 Instrumentation for AWS Lambda functions
 """
+
 import sys
-import wrapt
-import opentracing.ext.tags as ext
-
-from ...log import logger
-from ...singletons import env_is_aws_lambda
-from ... import get_lambda_handler_or_default
-from ...singletons import get_agent, get_tracer
-from .triggers import enrich_lambda_span, get_context
 import traceback
+from typing import TYPE_CHECKING, Any, Callable, Dict, Tuple
+
+import wrapt
+from opentelemetry.semconv.trace import SpanAttributes
+
+from instana import get_aws_lambda_handler
+from instana.instrumentation.aws.triggers import enrich_lambda_span, get_context
+from instana.log import logger
+from instana.singletons import env_is_aws_lambda, get_agent, get_tracer
+
+if TYPE_CHECKING:
+    from instana.agent.aws_lambda import AWSLambdaAgent
 
 
-def lambda_handler_with_instana(wrapped, instance, args, kwargs):
+def lambda_handler_with_instana(
+    wrapped: Callable[..., object],
+    instance: object,
+    args: Tuple[object, ...],
+    kwargs: Dict[str, Any],
+) -> object:
     event = args[0]
-    agent = get_agent()
+    agent: "AWSLambdaAgent" = get_agent()
     tracer = get_tracer()
 
     agent.collector.collect_snapshot(*args)
     incoming_ctx = get_context(tracer, event)
 
     result = None
-    with tracer.start_active_span("aws.lambda.entry", child_of=incoming_ctx) as scope:
-        enrich_lambda_span(agent, scope.span, *args)
+    with tracer.start_as_current_span(
+        "aws.lambda.entry", span_context=incoming_ctx
+    ) as span:
+        enrich_lambda_span(agent, span, *args)
         try:
             result = wrapped(*args, **kwargs)
 
             if isinstance(result, dict):
-                server_timing_value = "intid;desc=%s" % scope.span.context.trace_id
-                if 'headers' in result:
-                    result['headers']['Server-Timing'] = server_timing_value
-                elif 'multiValueHeaders' in result:
-                    result['multiValueHeaders']['Server-Timing'] = [server_timing_value]
-                if 'statusCode' in result and result.get('statusCode'):
-                    status_code = int(result['statusCode'])
-                    scope.span.set_tag(ext.HTTP_STATUS_CODE, status_code)
+                server_timing_value = f"intid;desc={span.context.trace_id}"
+                if "headers" in result:
+                    result["headers"]["Server-Timing"] = server_timing_value
+                elif "multiValueHeaders" in result:
+                    result["multiValueHeaders"]["Server-Timing"] = [server_timing_value]
+                if "statusCode" in result and result.get("statusCode"):
+                    status_code = int(result["statusCode"])
+                    span.set_attribute(SpanAttributes.HTTP_STATUS_CODE, status_code)
                     if 500 <= status_code:
-                        scope.span.log_exception(f'HTTP status {status_code}')
+                        span.record_exception(f"HTTP status {status_code}")
         except Exception as exc:
-            if scope.span:
+            logger.debug(f"AWS Lambda lambda_handler_with_instana error: {exc}")
+            if span:
                 exc = traceback.format_exc()
-                scope.span.log_exception(exc)
+                span.record_exception(exc)
             raise
         finally:
-            scope.close()
             agent.collector.shutdown()
 
-    agent.collector.shutdown()
+    if agent.collector.started:
+        agent.collector.shutdown()
+    
     return result
 
 
-if env_is_aws_lambda is True:
-    handler_module, handler_function = get_lambda_handler_or_default()
+if env_is_aws_lambda:
+    handler_module, handler_function = get_aws_lambda_handler()
 
-    if handler_module is not None and handler_function is not None:
+    if handler_module and handler_function:
         try:
-            logger.debug("Instrumenting AWS Lambda handler (%s.%s)" % (handler_module, handler_function))
-            sys.path.insert(0, '/var/runtime')
-            sys.path.insert(0, '/var/task')
-            wrapt.wrap_function_wrapper(handler_module, handler_function, lambda_handler_with_instana)
+            logger.debug(
+                f"Instrumenting AWS Lambda handler ({handler_module}.{handler_function})"
+            )
+            sys.path.insert(0, "/var/runtime")
+            sys.path.insert(0, "/var/task")
+            wrapt.wrap_function_wrapper(
+                handler_module, handler_function, lambda_handler_with_instana
+            )
         except (ModuleNotFoundError, ImportError) as exc:
-            logger.warning("Instana: Couldn't instrument AWS Lambda handler. Not monitoring.")
+            logger.debug(f"AWS Lambda error: {exc}")
+            logger.warning(
+                "Instana: Couldn't instrument AWS Lambda handler. Not monitoring."
+            )
     else:
-        logger.warning("Instana: Couldn't determine AWS Lambda Handler.  Not monitoring.")
+        logger.warning(
+            "Instana: Couldn't determine AWS Lambda Handler.  Not monitoring."
+        )

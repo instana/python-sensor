1b58bf4672e31fbdddb71060c6a0bbb1f56c3766
Varsha GS <varsha.gs@ibm.com>
2024-09-17 19:57:24 +0530

    pyramid: adapt tests after refactor
    (cherry picked from commit 126e4ec09440509da867e7b7aa49dfaf0c8a867d)
    Signed-off-by: Varsha GS <varsha.gs@ibm.com>

--- tests/apps/pyramid_app/__init__.py
+++ tests/apps/pyramid_app/__init__.py
@@ -2,10 +2,10 @@
 # (c) Copyright Instana Inc. 2020
 
 import os
-from .app import pyramid_server as server
-from ..utils import launch_background_thread
+from tests.apps.pyramid_app.app import pyramid_server as server
+from tests.apps.utils import launch_background_thread
 
 app_thread = None
 
-if not os.environ.get('CASSANDRA_TEST'):
+if not os.environ.get("CASSANDRA_TEST"):
     app_thread = launch_background_thread(server.serve_forever, "Pyramid")
--- tests/apps/pyramid_app/app.py
+++ tests/apps/pyramid_app/app.py
@@ -8,42 +8,50 @@ import logging
 from pyramid.response import Response
 import pyramid.httpexceptions as exc
 
-from ...helpers import testenv
+from tests.helpers import testenv
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 testenv["pyramid_port"] = 10815
-testenv["pyramid_server"] = ("http://127.0.0.1:" + str(testenv["pyramid_port"]))
+testenv["pyramid_server"] = "http://127.0.0.1:" + str(testenv["pyramid_port"])
+
 
 def hello_world(request):
-    return Response('Ok')
+    return Response("Ok")
+
 
 def please_fail(request):
     raise exc.HTTPInternalServerError("internal error")
 
+
 def tableflip(request):
     raise BaseException("fake exception")
 
+
 def response_headers(request):
-    headers = {
-        'X-Capture-This': 'Ok',
-        'X-Capture-That': 'Ok too'
-    }
+    headers = {"X-Capture-This": "Ok", "X-Capture-That": "Ok too"}
     return Response("Stan wuz here with headers!", headers=headers)
+
+
+def hello_user(request):
+    user = request.matchdict["user"]
+    return Response(f"Hello {user}!")
+
 
 app = None
 with Configurator() as config:
-    config.add_tween('instana.instrumentation.pyramid.tweens.InstanaTweenFactory')
-    config.add_route('hello', '/')
-    config.add_view(hello_world, route_name='hello')
-    config.add_route('fail', '/500')
-    config.add_view(please_fail, route_name='fail')
-    config.add_route('crash', '/exception')
-    config.add_view(tableflip, route_name='crash')
-    config.add_route('response_headers', '/response_headers')
-    config.add_view(response_headers, route_name='response_headers')
+    config.include("instana.instrumentation.pyramid.tweens")
+    config.add_route("hello", "/")
+    config.add_view(hello_world, route_name="hello")
+    config.add_route("fail", "/500")
+    config.add_view(please_fail, route_name="fail")
+    config.add_route("crash", "/exception")
+    config.add_view(tableflip, route_name="crash")
+    config.add_route("response_headers", "/response_headers")
+    config.add_view(response_headers, route_name="response_headers")
+    config.add_route("hello_user", "/hello_user/{user}")
+    config.add_view(hello_user, route_name="hello_user")
     app = config.make_wsgi_app()
-    
-pyramid_server = make_server('127.0.0.1', testenv["pyramid_port"], app)
 
+pyramid_server = make_server("127.0.0.1", testenv["pyramid_port"], app)

484af8f894b6dc1cc64f813a8b6b5ceb34dd7455
Varsha GS <varsha.gs@ibm.com>
2024-09-20 15:06:04 +0530

    pyramid: enable auto-instrumentation
    Signed-off-by: Varsha GS <varsha.gs@ibm.com>

--- tests/apps/pyramid/pyramid_app/__init__.py
+++ tests/apps/pyramid/pyramid_app/__init__.py
@@ -2,7 +2,7 @@
 # (c) Copyright Instana Inc. 2020
 
 import os
-from tests.apps.pyramid_app.app import pyramid_server as server
+from tests.apps.pyramid.pyramid_app.app import pyramid_server as server
 from tests.apps.utils import launch_background_thread
 
 app_thread = None

52f8aebf2b836aad2a0475f257b11925cd3ef5f2
Paulo Vital <paulo.vital@ibm.com>
2024-09-23 09:56:41 +0200

    style: format autoprofile files.
    Used ruff (vscode) to:
    - Black-compatible code formatting.
    - fix all auto-fixable violations, like unused imports.
    - isort-compatible import sorting.
    
    Signed-off-by: Paulo Vital <paulo.vital@ibm.com>

--- src/instana/autoprofile/samplers/allocation_sampler.py
+++ src/instana/autoprofile/samplers/allocation_sampler.py
@@ -3,62 +3,69 @@
 
 import threading
 
-from ...log import logger
-from ..runtime import min_version, runtime_info
-from ..profile import Profile
-from ..profile import CallSite
-from ..schedule import schedule, delay
+from instana.autoprofile.profile import CallSite, Profile
+from instana.autoprofile.runtime import RuntimeInfo, min_version
+from instana.autoprofile.schedule import schedule
+from instana.log import logger
 
 if min_version(3, 4):
     import tracemalloc
 
 
 class AllocationSampler(object):
-    MAX_TRACEBACK_SIZE = 25 # number of frames
-    MAX_MEMORY_OVERHEAD = 10 * 1e6 # 10MB
+    MAX_TRACEBACK_SIZE = 25  # number of frames
+    MAX_MEMORY_OVERHEAD = 10 * 1e6  # 10MB
     MAX_PROFILED_ALLOCATIONS = 25
 
-    def __init__(self, profiler):
+    def __init__(self, profiler: Profile) -> None:
         self.profiler = profiler
         self.ready = False
         self.top = None
         self.top_lock = threading.Lock()
         self.overhead_monitor = None
 
-    def setup(self):
-        if self.profiler.get_option('allocation_sampler_disabled'):
+    def setup(self) -> None:
+        if self.profiler.get_option("allocation_sampler_disabled"):
             return
 
-        if not runtime_info.OS_LINUX and not runtime_info.OS_DARWIN:
-            logger.debug('Allocation sampler is only supported on Linux and OS X.')
+        if not RuntimeInfo.OS_LINUX and not RuntimeInfo.OS_DARWIN:
+            logger.debug("Allocation sampler is only supported on Linux and OS X.")
             return
 
         if not min_version(3, 4):
-            logger.debug('Memory allocation profiling is available for Python 3.4 or higher')
+            logger.debug(
+                "Memory allocation profiling is available for Python 3.4 or higher."
+            )
             return
 
         self.ready = True
 
-    def reset(self):
-        self.top = CallSite('', '', 0)
+    def reset(self) -> None:
+        self.top = CallSite("", "", 0)
 
-    def start_sampler(self):
-        logger.debug('Activating memory allocation sampler.')
+    def start_sampler(self) -> None:
+        logger.debug("Activating memory allocation sampler.")
 
-        def start():
+        def start() -> None:
             tracemalloc.start(self.MAX_TRACEBACK_SIZE)
+
         self.profiler.run_in_main_thread(start)
 
-        def monitor_overhead():
-            if tracemalloc.is_tracing() and tracemalloc.get_tracemalloc_memory() > self.MAX_MEMORY_OVERHEAD:
-                logger.debug('Allocation sampler memory overhead limit exceeded: %s bytes', tracemalloc.get_tracemalloc_memory())
+        def monitor_overhead() -> None:
+            if (
+                tracemalloc.is_tracing()
+                and tracemalloc.get_tracemalloc_memory() > self.MAX_MEMORY_OVERHEAD
+            ):
+                logger.debug(
+                    f"Allocation sampler memory overhead limit exceeded: {tracemalloc.get_tracemalloc_memory()} bytes."
+                )
                 self.stop_sampler()
 
-        if not self.profiler.get_option('disable_timers'):
+        if not self.profiler.get_option("disable_timers"):
             self.overhead_monitor = schedule(0.5, 0.5, monitor_overhead)
 
-    def stop_sampler(self):
-        logger.debug('Deactivating memory allocation sampler.')
+    def stop_sampler(self) -> None:
+        logger.debug("Deactivating memory allocation sampler.")
 
         with self.top_lock:
             if self.overhead_monitor:
@@ -67,11 +74,13 @@ class AllocationSampler(object):
 
             if tracemalloc.is_tracing():
                 snapshot = tracemalloc.take_snapshot()
-                logger.debug('Allocation sampler memory overhead %s bytes', tracemalloc.get_tracemalloc_memory())
+                logger.debug(
+                    f"Allocation sampler memory overhead {tracemalloc.get_tracemalloc_memory()} bytes.",
+                )
                 tracemalloc.stop()
                 self.process_snapshot(snapshot)
 
-    def build_profile(self, duration, timespan):
+    def build_profile(self, duration: int, timespan: int) -> Profile:
         with self.top_lock:
             self.top.normalize(duration)
             self.top.floor()
@@ -82,22 +91,24 @@ class AllocationSampler(object):
                 Profile.UNIT_BYTE,
                 self.top.children.values(),
                 duration,
-                timespan
+                timespan,
             )
 
             return profile
 
-    def destroy(self):
+    def destroy(self) -> None:
         pass
 
-    def process_snapshot(self, snapshot):
-        stats = snapshot.statistics('traceback')
+    def process_snapshot(self, snapshot: tracemalloc.Snapshot) -> None:
+        stats = snapshot.statistics("traceback")
 
-        for stat in stats[:self.MAX_PROFILED_ALLOCATIONS]:
+        for stat in stats[: self.MAX_PROFILED_ALLOCATIONS]:
             if stat.traceback:
                 skip_stack = False
                 for frame in stat.traceback:
-                    if frame.filename and self.profiler.frame_cache.is_profiler_frame(frame.filename):
+                    if frame.filename and self.profiler.frame_cache.is_profiler_frame(
+                        frame.filename
+                    ):
                         skip_stack = True
                         break
                 if skip_stack:
@@ -105,8 +116,10 @@ class AllocationSampler(object):
 
                 current_node = self.top
                 for frame in reversed(stat.traceback):
-                    if frame.filename == '<unknown>':
+                    if frame.filename == "<unknown>":
                         continue
 
-                    current_node = current_node.find_or_add_child('', frame.filename, frame.lineno)
+                    current_node = current_node.find_or_add_child(
+                        "", frame.filename, frame.lineno
+                    )
                 current_node.increment(stat.size, stat.count)

fd82b2922e40dbb2c97837adcae1963610132b43
Paulo Vital <paulo.vital@ibm.com>
2024-09-24 16:58:51 +0200

    tests(propagators): adapt tests to OTel usage.
    Used ruff (vscode) to:
    - Black-compatible code formatting.
    - fix all auto-fixable violations, like unused imports.
    - isort-compatible import sorting.
    
    Signed-off-by: Paulo Vital <paulo.vital@ibm.com>

--- tests/propagators/test_binary_propagator.py
+++ tests/propagators/test_binary_propagator.py
@@ -1,73 +1,180 @@
 # (c) Copyright IBM Corp. 2021
 # (c) Copyright Instana Inc. 2021
 
+from typing import Generator
+
+import pytest
+from opentelemetry.trace import (
+    format_span_id,
+    format_trace_id,
+)
+
 from instana.propagators.binary_propagator import BinaryPropagator
 from instana.span_context import SpanContext
-import unittest
 
 
-class TestBinaryPropagator(unittest.TestCase):
-    def setUp(self):
+class TestBinaryPropagator:
+    @pytest.fixture(autouse=True)
+    def _resources(self) -> Generator[None, None, None]:
+        """SetUp and TearDown"""
+        # setup
         self.bp = BinaryPropagator()
+        yield
 
-    def test_inject_carrier_dict(self):
+    def test_inject_carrier_dict(self, trace_id: int, span_id: int) -> None:
         carrier = {}
-        ctx = SpanContext(span_id="1234567890abcdef", trace_id="1234d0e0e4736234",
-                                  level=1, baggage={}, sampled=True,
-                                  synthetic=False)
+        ctx = SpanContext(
+            span_id=span_id,
+            trace_id=trace_id,
+            is_remote=False,
+            level=1,
+            baggage={},
+            sampled=True,
+            synthetic=False,
+        )
         carrier = self.bp.inject(ctx, carrier)
-        self.assertEqual(carrier[b'x-instana-t'], b"1234d0e0e4736234")
 
-    def test_inject_carrier_dict_w3c_True(self):
+        assert carrier[b"x-instana-t"] == str(trace_id).encode("utf-8")
+        assert carrier[b"x-instana-s"] == str(span_id).encode("utf-8")
+        assert carrier[b"x-instana-l"] == b"1"
+        assert carrier[b"server-timing"] == f"intid;desc={trace_id}".encode("utf-8")
+
+    def test_inject_carrier_dict_w3c_True(self, trace_id: int, span_id: int) -> None:
         carrier = {}
-        ctx = SpanContext(span_id="1234567890abcdef", trace_id="1234d0e0e4736234",
-                                  level=1, baggage={}, sampled=True,
-                                  synthetic=False)
+        ctx = SpanContext(
+            span_id=span_id,
+            trace_id=trace_id,
+            is_remote=False,
+            level=1,
+            baggage={},
+            sampled=True,
+            synthetic=False,
+        )
         carrier = self.bp.inject(ctx, carrier, disable_w3c_trace_context=False)
-        self.assertEqual(carrier[b'x-instana-t'], b"1234d0e0e4736234")
-        self.assertEqual(carrier[b'traceparent'], b'00-00000000000000001234d0e0e4736234-1234567890abcdef-01')
-        self.assertEqual(carrier[b'tracestate'], b'in=1234d0e0e4736234;1234567890abcdef')
 
-    def test_inject_carrier_list(self):
+        assert carrier[b"x-instana-t"] == str(trace_id).encode("utf-8")
+        assert carrier[b"x-instana-s"] == str(span_id).encode("utf-8")
+        assert carrier[b"x-instana-l"] == b"1"
+        assert carrier[b"server-timing"] == f"intid;desc={trace_id}".encode("utf-8")
+        assert carrier[
+            b"traceparent"
+        ] == f"00-{format_trace_id(trace_id)}-{format_span_id(span_id)}-01".encode(
+            "utf-8"
+        )
+        assert carrier[b"tracestate"] == f"in={trace_id};{span_id}".encode("utf-8")
+
+    def test_inject_carrier_list(self, trace_id: int, span_id: int) -> None:
         carrier = []
-        ctx = SpanContext(span_id="1234567890abcdef", trace_id="1234d0e0e4736234",
-                                  level=1, baggage={}, sampled=True,
-                                  synthetic=False)
+        ctx = SpanContext(
+            span_id=span_id,
+            trace_id=trace_id,
+            is_remote=False,
+            level=1,
+            baggage={},
+            sampled=True,
+            synthetic=False,
+        )
         carrier = self.bp.inject(ctx, carrier)
-        self.assertEqual(carrier[0], (b'x-instana-t', b'1234d0e0e4736234'))
 
-    def test_inject_carrier_list_w3c_True(self):
+        assert isinstance(carrier, list)
+        assert carrier[0] == (b"x-instana-t", str(trace_id).encode("utf-8"))
+        assert carrier[1] == (b"x-instana-s", str(span_id).encode("utf-8"))
+        assert carrier[2] == (b"x-instana-l", b"1")
+        assert carrier[3] == (
+            b"server-timing",
+            f"intid;desc={trace_id}".encode("utf-8"),
+        )
+
+    def test_inject_carrier_list_w3c_True(self, trace_id: int, span_id: int) -> None:
         carrier = []
-        ctx = SpanContext(span_id="1234567890abcdef", trace_id="1234d0e0e4736234",
-                                  level=1, baggage={}, sampled=True,
-                                  synthetic=False)
+        ctx = SpanContext(
+            span_id=span_id,
+            trace_id=trace_id,
+            is_remote=False,
+            level=1,
+            baggage={},
+            sampled=True,
+            synthetic=False,
+        )
         carrier = self.bp.inject(ctx, carrier, disable_w3c_trace_context=False)
-        self.assertEqual(carrier[2], (b'x-instana-t', b'1234d0e0e4736234'))
-        self.assertEqual(carrier[0], (b'traceparent', b'00-00000000000000001234d0e0e4736234-1234567890abcdef-01'))
-        self.assertEqual(carrier[1], (b'tracestate', b'in=1234d0e0e4736234;1234567890abcdef'))
 
-    def test_inject_carrier_tupple(self):
+        assert isinstance(carrier, list)
+        assert carrier[0] == (
+            b"traceparent",
+            f"00-{format_trace_id(trace_id)}-{format_span_id(span_id)}-01".encode(
+                "utf-8"
+            ),
+        )
+        assert carrier[1] == (b"tracestate", f"in={trace_id};{span_id}".encode("utf-8"))
+        assert carrier[2] == (b"x-instana-t", str(trace_id).encode("utf-8"))
+        assert carrier[3] == (b"x-instana-s", str(span_id).encode("utf-8"))
+        assert carrier[4] == (b"x-instana-l", b"1")
+        assert carrier[5] == (
+            b"server-timing",
+            f"intid;desc={trace_id}".encode("utf-8"),
+        )
+
+    def test_inject_carrier_tuple(self, trace_id: int, span_id: int) -> None:
         carrier = ()
-        ctx = SpanContext(span_id="1234567890abcdef", trace_id="1234d0e0e4736234",
-                                  level=1, baggage={}, sampled=True,
-                                  synthetic=False)
+        ctx = SpanContext(
+            span_id=span_id,
+            trace_id=trace_id,
+            is_remote=False,
+            level=1,
+            baggage={},
+            sampled=True,
+            synthetic=False,
+        )
         carrier = self.bp.inject(ctx, carrier)
-        self.assertEqual(carrier[0], (b'x-instana-t', b'1234d0e0e4736234'))
 
-    def test_inject_carrier_tupple_w3c_True(self):
+        assert isinstance(carrier, tuple)
+        assert carrier[0] == (b"x-instana-t", str(trace_id).encode("utf-8"))
+        assert carrier[1] == (b"x-instana-s", str(span_id).encode("utf-8"))
+        assert carrier[2] == (b"x-instana-l", b"1")
+        assert carrier[3] == (
+            b"server-timing",
+            f"intid;desc={trace_id}".encode("utf-8"),
+        )
+
+    def test_inject_carrier_tuple_w3c_True(self, trace_id: int, span_id: int) -> None:
         carrier = ()
-        ctx = SpanContext(span_id="1234567890abcdef", trace_id="1234d0e0e4736234",
-                                  level=1, baggage={}, sampled=True,
-                                  synthetic=False)
+        ctx = SpanContext(
+            span_id=span_id,
+            trace_id=trace_id,
+            is_remote=False,
+            level=1,
+            baggage={},
+            sampled=True,
+            synthetic=False,
+        )
         carrier = self.bp.inject(ctx, carrier, disable_w3c_trace_context=False)
-        self.assertEqual(carrier[2], (b'x-instana-t', b'1234d0e0e4736234'))
-        self.assertEqual(carrier[0], (b'traceparent', b'00-00000000000000001234d0e0e4736234-1234567890abcdef-01'))
-        self.assertEqual(carrier[1], (b'tracestate', b'in=1234d0e0e4736234;1234567890abcdef'))
 
-    def test_inject_carrier_set_exception(self):
+        assert isinstance(carrier, tuple)
+        assert carrier[0] == (
+            b"traceparent",
+            f"00-{format_trace_id(trace_id)}-{format_span_id(span_id)}-01".encode(
+                "utf-8"
+            ),
+        )
+        assert carrier[1] == (b"tracestate", f"in={trace_id};{span_id}".encode("utf-8"))
+        assert carrier[2] == (b"x-instana-t", str(trace_id).encode("utf-8"))
+        assert carrier[3] == (b"x-instana-s", str(span_id).encode("utf-8"))
+        assert carrier[4] == (b"x-instana-l", b"1")
+        assert carrier[5] == (
+            b"server-timing",
+            f"intid;desc={trace_id}".encode("utf-8"),
+        )
+
+    def test_inject_carrier_set_exception(self, trace_id: int, span_id: int) -> None:
         carrier = set()
-        ctx = SpanContext(span_id="1234567890abcdef", trace_id="1234d0e0e4736234",
-                                  level=1, baggage={}, sampled=True,
-                                  synthetic=False)
+        ctx = SpanContext(
+            span_id=span_id,
+            trace_id=trace_id,
+            is_remote=False,
+            level=1,
+            baggage={},
+            sampled=True,
+            synthetic=False,
+        )
         carrier = self.bp.inject(ctx, carrier)
-        self.assertIsNone(carrier)
+        assert not carrier

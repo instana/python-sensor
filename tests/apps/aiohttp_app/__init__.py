fae1d235a1cfb76c27b5f10cd6b723df989accd6
Varsha GS <varsha.gs@ibm.com>
2024-10-08 09:59:25 +0530

    fix(currency): run only starlette tests in `gevent_starlette` task
    Signed-off-by: Varsha GS <varsha.gs@ibm.com>

--- tests/apps/aiohttp_app/__init__.py
+++ tests/apps/aiohttp_app/__init__.py
@@ -8,7 +8,7 @@ from ..utils import launch_background_thread
 
 APP_THREAD = None
 
-if not any((os.environ.get('GEVENT_STARLETTE_TEST'),
+if not any((os.environ.get('GEVENT_TEST'),
             os.environ.get('CASSANDRA_TEST'),
             sys.version_info < (3, 5, 3))):
     APP_THREAD = launch_background_thread(server, "AIOHTTP")
--- tests/apps/aiohttp_app2/__init__.py
+++ tests/apps/aiohttp_app2/__init__.py
@@ -7,7 +7,7 @@ from tests.apps.utils import launch_background_thread
 
 APP_THREAD = None
 
-if not any((os.environ.get('GEVENT_STARLETTE_TEST'),
+if not any((os.environ.get('GEVENT_TEST'),
             os.environ.get('CASSANDRA_TEST'),
             sys.version_info < (3, 5, 3))):
     APP_THREAD = launch_background_thread(server, "AIOHTTP")

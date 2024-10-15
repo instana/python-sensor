3b62879e91cbd68e40a99198f3e5b7444662cb58
Cagri Yonca <cagri@ibm.com>
2024-10-15 05:13:09 -0700

    refactor(hook_uwsgi): added uwsgi support to otel
    Signed-off-by: Cagri Yonca <cagri@ibm.com>

--- src/instana/hooks/hook_uwsgi.py
+++ src/instana/hooks/hook_uwsgi.py
@@ -7,34 +7,45 @@ when running under uWSGI.  Here we attempt to detect the presence of these packa
 then use the appropriate hooks.
 """
 
-from ..log import logger
-from ..singletons import agent
-
 try:
+    from instana.log import logger
+    from instana.singletons import agent
+
     import uwsgi
-    logger.debug("uWSGI options: %s", uwsgi.opt)
 
-    opt_master = uwsgi.opt.get('master', False)
-    opt_lazy_apps = uwsgi.opt.get('lazy-apps', False)
+    logger.debug(
+        f"uWSGI options: {uwsgi.opt}",
+    )
 
-    if uwsgi.opt.get('enable-threads', False) is False and uwsgi.opt.get('gevent', False) is False:
-        logger.warning("Required: Neither uWSGI threads or gevent is enabled. " +
-                    "Please enable by using the uWSGI --enable-threads or --gevent option.")
+    opt_master = uwsgi.opt.get("master", False)
+    opt_lazy_apps = uwsgi.opt.get("lazy-apps", False)
 
-    if opt_master and opt_lazy_apps is False:
+    if not uwsgi.opt.get("enable-threads", False) and not uwsgi.opt.get(
+        "gevent", False
+    ):
+        logger.warning(
+            "Required: Neither uWSGI threads or gevent is enabled. "
+            + "Please enable by using the uWSGI --enable-threads or --gevent option."
+        )
+
+    if opt_master and not opt_lazy_apps:
         # --master is supplied in uWSGI options (otherwise uwsgidecorators package won't be available)
         # When --lazy-apps is True, this postfork hook isn't needed
         import uwsgidecorators
 
         @uwsgidecorators.postfork
-        def uwsgi_handle_fork():
-            """ This is our uWSGI hook to detect and act when worker processes are forked off. """
+        def uwsgi_handle_fork() -> None:
+            """This is our uWSGI hook to detect and act when worker processes are forked off."""
             logger.debug("Handling uWSGI fork...")
             agent.handle_fork()
 
         logger.debug("Applied uWSGI hooks")
     else:
-        logger.debug("uWSGI --master=%s --lazy-apps=%s: postfork hooks not applied", opt_master, opt_lazy_apps)
+        logger.debug(
+            f"uWSGI --master={opt_master} --lazy-apps={opt_lazy_apps}: postfork hooks not applied"
+        )
 except ImportError:
-    logger.debug('uwsgi hooks: decorators not available: likely not running under uWSGI')
+    logger.debug(
+        "uwsgi hooks: decorators not available: likely not running under uWSGI"
+    )
     pass

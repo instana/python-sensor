aa616cbc00bd44913a86549751b0d82c027a4eec
Varsha GS <varsha.gs@ibm.com>
2024-11-06 10:41:32 +0530

    fix: add new aws region `ap-southeast-5` to publish
    Signed-off-by: Varsha GS <varsha.gs@ibm.com>

--- bin/aws-lambda/build_and_publish_lambda_layer.py
+++ bin/aws-lambda/build_and_publish_lambda_layer.py
@@ -97,6 +97,7 @@ else:
                'ap-southeast-2',
                'ap-southeast-3',
                'ap-southeast-4',
+               'ap-southeast-5',
                'ca-central-1',
                'ca-west-1',
                'cn-north-1',

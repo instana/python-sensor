bef2194e8025da7c26eeccdcb5bb20165fe13508
Varsha GS <varsha.gs@ibm.com>
2024-12-30 14:25:29 +0530

    test(flask, starlette): cover testcases to capture http headers
    Signed-off-by: Varsha GS <varsha.gs@ibm.com>

--- tests/frameworks/test_starlette.py
+++ tests/frameworks/test_starlette.py
@@ -23,6 +23,8 @@ class TestStarlette:
         agent.options.extra_http_headers = [
             "X-Capture-This",
             "X-Capture-That",
+            "X-Capture-This-Too",
+            "X-Capture-That-Too"
         ]
         # Clear all spans before a test run.
         self.recorder = tracer.span_processor
@@ -245,7 +247,7 @@ class TestStarlette:
         assert asgi_span.sy
         assert not test_span.sy
 
-    def test_custom_header_capture(self) -> None:
+    def test_request_header_capture(self) -> None:
         with tracer.start_as_current_span("test") as span:
             # As TestClient() is based on httpx, and we don't support it yet,
             # we must pass the SDK trace_id and span_id to the ASGI server.
@@ -299,3 +301,56 @@ class TestStarlette:
         assert "this" == asgi_span.data["http"]["header"]["X-Capture-This"]
         assert "X-Capture-That" in asgi_span.data["http"]["header"]
         assert "that" == asgi_span.data["http"]["header"]["X-Capture-That"]
+
+    def test_response_header_capture(self) -> None:
+        with tracer.start_as_current_span("test") as span:
+            # As TestClient() is based on httpx, and we don't support it yet,
+            # we must pass the SDK trace_id and span_id to the ASGI server.
+            span_context = span.get_span_context()
+            headers = {
+                "X-INSTANA-T": hex_id(span_context.trace_id),
+                "X-INSTANA-S": hex_id(span_context.span_id),
+            }
+            result = self.client.get("/response_headers", headers=headers)
+
+        assert result
+        assert "X-INSTANA-T" in result.headers
+        assert "X-INSTANA-S" in result.headers
+        assert "X-INSTANA-L" in result.headers
+        assert "Server-Timing" in result.headers
+        assert result.headers["X-INSTANA-L"] == "1"
+
+        spans = self.recorder.queued_spans()
+        assert len(spans) == 2
+
+        span_filter = (  # noqa: E731
+            lambda span: span.n == "sdk" and span.data["sdk"]["name"] == "test"
+        )
+        test_span = get_first_span_by_filter(spans, span_filter)
+        assert test_span
+
+        span_filter = lambda span: span.n == "asgi"  # noqa: E731
+        asgi_span = get_first_span_by_filter(spans, span_filter)
+        assert asgi_span
+
+        assert test_span.t == asgi_span.t
+        assert test_span.s == asgi_span.p
+
+        assert result.headers["X-INSTANA-T"] == hex_id(asgi_span.t)
+        assert result.headers["X-INSTANA-S"] == hex_id(asgi_span.s)
+        assert result.headers["X-INSTANA-L"] == "1"
+        assert result.headers["Server-Timing"] == f"intid;desc={hex_id(asgi_span.t)}"
+
+        assert not asgi_span.ec
+        assert asgi_span.data["http"]["host"] == "testserver"
+        assert asgi_span.data["http"]["path"] == "/response_headers"
+        assert asgi_span.data["http"]["path_tpl"] == "/response_headers"
+        assert asgi_span.data["http"]["method"] == "GET"
+        assert asgi_span.data["http"]["status"] == 200
+        assert not asgi_span.data["http"]["error"]
+        assert not asgi_span.data["http"]["params"]
+
+        assert "X-Capture-This-Too" in asgi_span.data["http"]["header"]
+        assert "this too" == asgi_span.data["http"]["header"]["X-Capture-This-Too"]
+        assert "X-Capture-That-Too" in asgi_span.data["http"]["header"]
+        assert "that too" == asgi_span.data["http"]["header"]["X-Capture-That-Too"]

74a0bdb6ea490cfe73eb32364ed358d0624d22b5
Varsha GS <varsha.gs@ibm.com>
2025-01-13 10:14:39 +0530

    tests(traceutils): cover all supported header formats
    Signed-off-by: Varsha GS <varsha.gs@ibm.com>

--- tests/util/test_traceutils.py
+++ tests/util/test_traceutils.py
@@ -1,6 +1,6 @@
 # (c) Copyright IBM Corp. 2024
 
-from unittest.mock import patch
+import pytest
 
 from instana.singletons import agent, tracer
 from instana.tracer import InstanaTracer
@@ -12,26 +12,61 @@ from instana.util.traceutils import (
 )
 
 
-def test_extract_custom_headers(span) -> None:
+@pytest.mark.parametrize(
+    "custom_headers, format",
+    [
+        (
+            {
+                "X-Capture-This-Too": "this too",
+                "X-Capture-That-Too": "that too",
+            },
+            False,
+        ),
+        (
+            {
+                "HTTP_X_CAPTURE_THIS_TOO": "this too",
+                "HTTP_X_CAPTURE_THAT_TOO": "that too",
+            },
+            True,
+        ),
+        (
+            [("X-CAPTURE-THIS-TOO", "this too"), ("x-capture-that-too", "that too")],
+            False,
+        ),
+        (
+            [
+                (b"X-Capture-This-Too", b"this too"),
+                (b"X-Capture-That-Too", b"that too"),
+            ],
+            False,
+        ),
+        (
+            [
+                ("HTTP_X_CAPTURE_THIS_TOO", "this too"),
+                ("HTTP_X_CAPTURE_THAT_TOO", "that too"),
+            ],
+            True,
+        ),
+    ],
+)
+def test_extract_custom_headers(span, custom_headers, format) -> None:
     agent.options.extra_http_headers = ["X-Capture-This-Too", "X-Capture-That-Too"]
-    request_headers = {
-        "X-Capture-This-Too": "this too",
-        "X-Capture-That-Too": "that too",
-    }
-    extract_custom_headers(span, request_headers)
+    extract_custom_headers(span, custom_headers, format=format)
     assert len(span.attributes) == 2
     assert span.attributes["http.header.X-Capture-This-Too"] == "this too"
     assert span.attributes["http.header.X-Capture-That-Too"] == "that too"
 
 
-def test_get_activate_tracer() -> None:
+def test_get_activate_tracer(mocker) -> None:
     assert not get_active_tracer()
 
     with tracer.start_as_current_span("test"):
         response = get_active_tracer()
         assert isinstance(response, InstanaTracer)
         assert response == tracer
-        with patch("instana.span.span.InstanaSpan.is_recording", return_value=False):
+        with mocker.patch(
+            "instana.span.span.InstanaSpan.is_recording", return_value=False
+        ):
             assert not get_active_tracer()
 
 

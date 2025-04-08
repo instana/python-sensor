bad19c0a39b8646e9333dcc9beba4b9b75950f96
Varsha GS <varsha.gs@ibm.com>
2025-04-08 15:37:19 +0530

    remove opt and check
    Signed-off-by: Varsha GS <varsha.gs@ibm.com>

--- src/instana/hooks/hook_uwsgi.py
+++ src/instana/hooks/hook_uwsgi.py
@@ -15,37 +15,37 @@ try:
     logger.debug("HEREEE")
     logger.debug(dir(uwsgi))
 
-    logger.debug(
-        f"uWSGI options: {uwsgi.opt}",
-    )
+    # logger.debug(
+    #     f"uWSGI options: {uwsgi.opt}",
+    # )
 
-    opt_master = uwsgi.opt.get("master", False)
-    opt_lazy_apps = uwsgi.opt.get("lazy-apps", False)
+    # opt_master = uwsgi.opt.get("master", False)
+    # opt_lazy_apps = uwsgi.opt.get("lazy-apps", False)
 
-    if not uwsgi.opt.get("enable-threads", False) and not uwsgi.opt.get(
-        "gevent", False
-    ):
-        logger.warning(
-            "Required: Neither uWSGI threads or gevent is enabled. "
-            + "Please enable by using the uWSGI --enable-threads or --gevent option."
-        )
+    # if not uwsgi.opt.get("enable-threads", False) and not uwsgi.opt.get(
+    #     "gevent", False
+    # ):
+    #     logger.warning(
+    #         "Required: Neither uWSGI threads or gevent is enabled. "
+    #         + "Please enable by using the uWSGI --enable-threads or --gevent option."
+    #     )
 
-    if opt_master and not opt_lazy_apps:
-        # --master is supplied in uWSGI options (otherwise uwsgidecorators package won't be available)
-        # When --lazy-apps is True, this postfork hook isn't needed
-        import uwsgidecorators
+    # if opt_master and not opt_lazy_apps:
+    # --master is supplied in uWSGI options (otherwise uwsgidecorators package won't be available)
+    # When --lazy-apps is True, this postfork hook isn't needed
+    import uwsgidecorators
 
-        @uwsgidecorators.postfork
-        def uwsgi_handle_fork() -> None:
-            """This is our uWSGI hook to detect and act when worker processes are forked off."""
-            logger.debug("Handling uWSGI fork...")
-            agent.handle_fork()
+    @uwsgidecorators.postfork
+    def uwsgi_handle_fork() -> None:
+        """This is our uWSGI hook to detect and act when worker processes are forked off."""
+        logger.debug("Handling uWSGI fork...")
+        agent.handle_fork()
 
-        logger.debug("Applied uWSGI hooks")
-    else:
-        logger.debug(
-            f"uWSGI --master={opt_master} --lazy-apps={opt_lazy_apps}: postfork hooks not applied"
-        )
+    logger.debug("Applied uWSGI hooks")
+    # else:
+    #     logger.debug(
+    #         f"uWSGI --master={opt_master} --lazy-apps={opt_lazy_apps}: postfork hooks not applied"
+    #     )
 except ImportError:
     logger.debug(
         "uwsgi hooks: decorators not available: likely not running under uWSGI"

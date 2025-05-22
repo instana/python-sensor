2c028008d3f643c586eb7be931a88af4ce9db11d
Varsha GS <varsha.gs@ibm.com>
2025-05-22 20:10:07 +0530

    chore: Add Type Annotations
    Signed-off-by: Varsha GS <varsha.gs@ibm.com>

--- src/instana/instrumentation/aio_pika.py
+++ src/instana/instrumentation/aio_pika.py
@@ -3,13 +3,31 @@
 try:
     import aio_pika
     import wrapt
+    from typing import (
+        TYPE_CHECKING,
+        Dict,
+        Any,
+        Callable,
+        Tuple,
+        Type,
+        Optional,
+    )
 
     from instana.log import logger
     from instana.propagators.format import Format
     from instana.util.traceutils import get_tracer_tuple, tracing_is_off
     from instana.singletons import tracer
 
-    def _extract_span_attributes(span, connection, sort, routing_key, exchange) -> None:
+    if TYPE_CHECKING:
+        from instana.span.span import InstanaSpan
+        from aio_pika.exchange import Exchange
+        from aiormq.abc import ConfirmationFrameType
+        from aio_pika.abc import ConsumerTag, AbstractMessage
+        from aio_pika.queue import Queue, QueueIterator
+
+    def _extract_span_attributes(
+        span: "InstanaSpan", connection, sort: str, routing_key: str, exchange: str
+    ) -> None:
         span.set_attribute("address", str(connection.url))
 
         span.set_attribute("sort", sort)
@@ -17,7 +35,12 @@ try:
         span.set_attribute("exchange", exchange)
 
     @wrapt.patch_function_wrapper("aio_pika", "Exchange.publish")
-    async def publish_with_instana(wrapped, instance, args, kwargs):
+    async def publish_with_instana(
+        wrapped: Callable[..., Optional["ConfirmationFrameType"]],
+        instance: "Exchange",
+        args: Tuple[object],
+        kwargs: Dict[str, Any],
+    ) -> Optional["ConfirmationFrameType"]:
         if tracing_is_off():
             return await wrapped(*args, **kwargs)
 
@@ -47,12 +70,22 @@ try:
                 return response
 
     @wrapt.patch_function_wrapper("aio_pika", "Queue.consume")
-    async def consume_with_instana(wrapped, instance, args, kwargs):
+    async def consume_with_instana(
+        wrapped: Callable[..., "ConsumerTag"],
+        instance: Type["Queue"],
+        args: Tuple[object],
+        kwargs: Dict[str, Any],
+    ) -> "ConsumerTag":
         connection = instance.channel._connection
         callback = kwargs["callback"] if kwargs.get("callback") else args[0]
 
         @wrapt.decorator
-        async def callback_wrapper(wrapped, instance, args, kwargs):
+        async def callback_wrapper(
+            wrapped: Callable[[Type["AbstractMessage"]], Any],
+            instance: Type["QueueIterator"],
+            args: Tuple[Type["AbstractMessage"], ...],
+            kwargs: Dict[str, Any],
+        ) -> Callable[[Type["AbstractMessage"]], Any]:
             message = args[0]
             parent_context = tracer.extract(
                 Format.HTTP_HEADERS, message.headers, disable_w3c_trace_context=True
@@ -60,7 +93,9 @@ try:
             with tracer.start_as_current_span(
                 "rabbitmq", span_context=parent_context
             ) as span:
-                _extract_span_attributes(span, connection, "consume", message.routing_key, message.exchange)
+                _extract_span_attributes(
+                    span, connection, "consume", message.routing_key, message.exchange
+                )
             try:
                 response = await wrapped(*args, **kwargs)
             except Exception as exc:
@@ -75,7 +110,7 @@ try:
             args = (wrapped_callback,) + args[1:]
 
         return await wrapped(*args, **kwargs)
-    
+
     logger.debug("Instrumenting aio-pika")
 
 except ImportError:

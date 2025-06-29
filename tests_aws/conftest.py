19feb9fb6d2bdd5da7c709d8612b2f90e4c7507b
Paulo Vital <paulo.vital@ibm.com>
2025-06-29 20:46:15 +0200

    fix (tests): Skipping tests not supported on ppc64.
    Signed-off-by: Paulo Vital <paulo.vital@ibm.com>

--- tests_aws/conftest.py
+++ tests_aws/conftest.py
@@ -2,6 +2,13 @@
 # (c) Copyright Instana Inc. 2020
 
 import os
+import platform
 
 os.environ["INSTANA_ENDPOINT_URL"] = "https://localhost/notreal"
 os.environ["INSTANA_AGENT_KEY"] = "Fake_Key"
+
+# ppc64le is not supported by AWS Lambda.
+collect_ignore_glob = []
+if platform.machine() == "ppc64le":
+    collect_ignore_glob.append("*test_grpcio*")
+    collect_ignore_glob.append("*test_google-cloud*")

929874bc1a51ea3e1e0caccd0a8fe91d43256be1
Paulo Vital <paulo.vital@ibm.com>
2025-06-29 20:46:15 +0200

    fix (tests): Skipping tests not supported on ppc64.
    The following tests are not executed in a ppc64le environment due to
    lack of support to run them:
    
    - grpcio: not installing in ppc64le.
    - google-cloud-*: depends on grpcio.
    - pymongo: only the Enterprise edition is supported in ppc64le.
    
    Signed-off-by: Paulo Vital <paulo.vital@ibm.com>

--- tests_aws/conftest.py
+++ tests_aws/conftest.py
@@ -2,6 +2,14 @@
 # (c) Copyright Instana Inc. 2020
 
 import os
+import platform
 
 os.environ["INSTANA_ENDPOINT_URL"] = "https://localhost/notreal"
 os.environ["INSTANA_AGENT_KEY"] = "Fake_Key"
+
+# ppc64le is not supported by AWS Serverless Services.
+collect_ignore_glob = []
+if platform.machine() == "ppc64le":
+    collect_ignore_glob.append("*test_lambda*")
+    collect_ignore_glob.append("*test_fargate*")
+    collect_ignore_glob.append("*test_eks*")

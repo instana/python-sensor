c1192225faba7c1b2981d430231bb3482592bed1
Paulo Vital <paulo.vital@ibm.com>
2025-07-29 23:58:59 +0200

    refactor: ConfigReader
    Signed-off-by: Paulo Vital <paulo.vital@ibm.com>

--- tests/util/test_config_reader.py
+++ tests/util/test_config_reader.py
@@ -2,19 +2,77 @@
 
 import logging
 import os
+from typing import TYPE_CHECKING, Generator
 
 import pytest
+from yaml import YAMLError
 
 from instana.util.config import (
     get_disable_trace_configurations_from_yaml,
     parse_ignored_endpoints_from_yaml,
 )
+from instana.util.config_reader import ConfigReader
+
+if TYPE_CHECKING:
+    from pytest import LogCaptureFixture
+    from pytest_mock import MockerFixture
 
 
 class TestConfigReader:
-    def test_load_configuration_with_tracing(
-        self, caplog: pytest.LogCaptureFixture
+    @pytest.fixture(autouse=True)
+    def _resource(
+        self,
+        caplog: "LogCaptureFixture",
+    ) -> Generator[None, None, None]:
+        yield
+        caplog.clear()
+        if "INSTANA_CONFIG_PATH" in os.environ:
+            os.environ.pop("INSTANA_CONFIG_PATH")
+
+    def test_config_reader_null(self, caplog: "LogCaptureFixture") -> None:
+        config_reader = ConfigReader(os.environ.get("INSTANA_CONFIG_PATH", ""))
+        assert config_reader.file_path == ""
+        assert config_reader.data == {}
+        assert "ConfigReader: No configuration file specified" in caplog.messages
+
+    def test_config_reader_default(self) -> None:
+        filename = "tests/util/test_configuration-1.yaml"
+        os.environ["INSTANA_CONFIG_PATH"] = filename
+        config_reader = ConfigReader(os.environ.get("INSTANA_CONFIG_PATH", ""))
+        assert config_reader.file_path == filename
+        assert "tracing" in config_reader.data
+        assert len(config_reader.data["tracing"]) == 2
+
+    def test_config_reader_file_not_found_error(
+        self, caplog: "LogCaptureFixture"
     ) -> None:
+        filename = "tests/util/test_configuration-3.yaml"
+        os.environ["INSTANA_CONFIG_PATH"] = filename
+        config_reader = ConfigReader(os.environ.get("INSTANA_CONFIG_PATH", ""))
+        assert config_reader.file_path == filename
+        assert config_reader.data == {}
+        assert (
+            f"ConfigReader: Configuration file has not found: {filename}"
+            in caplog.messages
+        )
+
+    def test_config_reader_yaml_error(
+        self, caplog: "LogCaptureFixture", mocker: "MockerFixture"
+    ) -> None:
+        filename = "tests/util/test_configuration-1.yaml"
+        exception_message = "BLAH"
+        mocker.patch(
+            "instana.util.config_reader.yaml.safe_load",
+            side_effect=YAMLError(exception_message),
+        )
+
+        config_reader = ConfigReader(filename)  # noqa: F841
+        assert (
+            f"ConfigReader: Error parsing YAML file: {exception_message}"
+            in caplog.messages
+        )
+
+    def test_load_configuration_with_tracing(self, caplog: "LogCaptureFixture") -> None:
         caplog.set_level(logging.DEBUG, logger="instana")
 
         ignore_endpoints = parse_ignored_endpoints_from_yaml(
@@ -49,7 +107,7 @@ class TestConfigReader:
             not in caplog.messages
         )
 
-    def test_load_configuration_legacy(self, caplog: pytest.LogCaptureFixture) -> None:
+    def test_load_configuration_legacy(self, caplog: "LogCaptureFixture") -> None:
         caplog.set_level(logging.DEBUG, logger="instana")
 
         ignore_endpoints = parse_ignored_endpoints_from_yaml(

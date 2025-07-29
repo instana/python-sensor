5f7b80cbc55f6ba2d8646dee9fc268a1081228ec
Paulo Vital <paulo.vital@ibm.com>
2025-07-29 23:58:59 +0200

    refactor: ConfigReader
    Signed-off-by: Paulo Vital <paulo.vital@ibm.com>

--- src/instana/util/config_reader.py
+++ src/instana/util/config_reader.py
@@ -1,15 +1,18 @@
 # (c) Copyright IBM Corp. 2025
 
-from typing import Union
-from instana.log import logger
 import yaml
+
+from instana.log import logger
 
 
 class ConfigReader:
-    def __init__(self, file_path: Union[str]) -> None:
+    def __init__(self, file_path: str) -> None:
         self.file_path = file_path
-        self.data = None
-        self.load_file()
+        self.data = {}
+        if file_path:
+            self.load_file()
+        else:
+            logger.warning("ConfigReader: No configuration file specified")
 
     def load_file(self) -> None:
         """Loads and parses the YAML file"""
@@ -17,6 +20,8 @@ class ConfigReader:
             with open(self.file_path, "r") as file:
                 self.data = yaml.safe_load(file)
         except FileNotFoundError:
-            logger.error(f"Configuration file has not found: {self.file_path}")
+            logger.error(
+                f"ConfigReader: Configuration file has not found: {self.file_path}"
+            )
         except yaml.YAMLError as e:
-            logger.error(f"Error parsing YAML file: {e}")
+            logger.error(f"ConfigReader: Error parsing YAML file: {e}")

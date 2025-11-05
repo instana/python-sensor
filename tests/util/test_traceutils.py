6ce4c3e688b9e5b5b1353e0abacc5319d44714bf
Cagri Yonca <cagri@ibm.com>
2025-11-05 17:20:46 +0100

    fix: Improved getting active tracer and current span mechanism
    Signed-off-by: Cagri Yonca <cagri@ibm.com>

--- tests/util/test_traceutils.py
+++ tests/util/test_traceutils.py
@@ -3,12 +3,9 @@
 import pytest
 
 from instana.singletons import agent, tracer
-from instana.tracer import InstanaTracer
 from instana.util.traceutils import (
     extract_custom_headers,
-    get_active_tracer,
     get_tracer_tuple,
-    tracing_is_off,
 )
 
 
@@ -57,19 +54,6 @@ def test_extract_custom_headers(span, custom_headers, format) -> None:
     assert span.attributes["http.header.X-Capture-That-Too"] == "that too"
 
 
-def test_get_activate_tracer(mocker) -> None:
-    assert not get_active_tracer()
-
-    with tracer.start_as_current_span("test"):
-        response = get_active_tracer()
-        assert isinstance(response, InstanaTracer)
-        assert response == tracer
-        with mocker.patch(
-            "instana.span.span.InstanaSpan.is_recording", return_value=False
-        ):
-            assert not get_active_tracer()
-
-
 def test_get_tracer_tuple() -> None:
     response = get_tracer_tuple()
     assert response == (None, None, None)
@@ -82,16 +66,3 @@ def test_get_tracer_tuple() -> None:
     with tracer.start_as_current_span("test") as span:
         response = get_tracer_tuple()
         assert response == (tracer, span, span.name)
-
-
-def test_tracing_is_off() -> None:
-    response = tracing_is_off()
-    assert response
-    with tracer.start_as_current_span("test"):
-        response = tracing_is_off()
-        assert not response
-
-    agent.options.allow_exit_as_root = True
-    response = tracing_is_off()
-    assert not response
-    agent.options.allow_exit_as_root = False

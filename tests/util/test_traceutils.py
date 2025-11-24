28935d94d6ba589c62b9db9586c3e02f5b59bd8d
Cagri Yonca <cagri@ibm.com>
2025-11-24 12:25:00 +0100

    fix: Improved getting active tracer and current span mechanism
    Signed-off-by: Cagri Yonca <cagri@ibm.com>

--- tests/util/test_traceutils.py
+++ tests/util/test_traceutils.py
@@ -5,12 +5,9 @@ from typing import Generator
 import pytest
 
 from instana.singletons import agent, get_tracer
-from instana.tracer import InstanaTracer
 from instana.util.traceutils import (
     extract_custom_headers,
-    get_active_tracer,
     get_tracer_tuple,
-    tracing_is_off,
 )
 
 
@@ -66,21 +63,7 @@ class TestTraceutils:
         assert span.attributes["http.header.X-Capture-This-Too"] == "this too"
         assert span.attributes["http.header.X-Capture-That-Too"] == "that too"
 
-    def test_get_activate_tracer(self, mocker) -> None:
-        assert not get_active_tracer()
-
-        with self.tracer.start_as_current_span("test"):
-            response = get_active_tracer()
-            assert isinstance(response, InstanaTracer)
-            assert response == self.tracer
-            with mocker.patch(
-                "instana.span.span.InstanaSpan.is_recording", return_value=False
-            ):
-                assert not get_active_tracer()
-
-    def test_get_tracer_tuple(
-        self,
-    ) -> None:
+    def test_get_tracer_tuple(self) -> None:
         response = get_tracer_tuple()
         assert response == (None, None, None)
 
@@ -92,15 +75,3 @@ class TestTraceutils:
         with self.tracer.start_as_current_span("test") as span:
             response = get_tracer_tuple()
             assert response == (self.tracer, span, span.name)
-
-    def test_tracing_is_off(self) -> None:
-        response = tracing_is_off()
-        assert response
-        with self.tracer.start_as_current_span("test"):
-            response = tracing_is_off()
-            assert not response
-
-        agent.options.allow_exit_as_root = True
-        response = tracing_is_off()
-        assert not response
-        agent.options.allow_exit_as_root = False

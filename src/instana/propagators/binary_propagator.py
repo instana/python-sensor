ef22e8f2c4193a924b736fd5c63e01bf3cd48e70
Paulo Vital <paulo.vital@ibm.com>
2026-03-20 14:39:11 +0100

    style(fsm): format Propagators
    Added type annotations to the binary, HTTP and text propagators and used ruff (vscode) to:
    - Black-compatible code formatting.
    - fix all auto-fixable violations, like unused imports.
    - isort-compatible import sorting.
    
    Signed-off-by: Paulo Vital

--- src/instana/propagators/binary_propagator.py
+++ src/instana/propagators/binary_propagator.py
@@ -2,11 +2,13 @@
 # (c) Copyright Instana Inc. 2020
 
 
-from instana.log import logger
-from instana.propagators.base_propagator import BasePropagator
+from typing import Optional
 
 from opentelemetry.trace.span import format_span_id
 
+from instana.log import logger
+from instana.propagators.base_propagator import BasePropagator, CarrierT
+from instana.span_context import SpanContext
 from instana.util.ids import define_server_timing
 
 
@@ -17,17 +19,22 @@ class BinaryPropagator(BasePropagator):
     """
 
     # ByteArray variations from base class
-    HEADER_KEY_T = b'x-instana-t'
-    HEADER_KEY_S = b'x-instana-s'
-    HEADER_KEY_L = b'x-instana-l'
-    HEADER_SERVER_TIMING = b'server-timing'
-    HEADER_KEY_TRACEPARENT = b'traceparent'
-    HEADER_KEY_TRACESTATE = b'tracestate'
+    HEADER_KEY_T = b"x-instana-t"
+    HEADER_KEY_S = b"x-instana-s"
+    HEADER_KEY_L = b"x-instana-l"
+    HEADER_SERVER_TIMING = b"server-timing"
+    HEADER_KEY_TRACEPARENT = b"traceparent"
+    HEADER_KEY_TRACESTATE = b"tracestate"
 
-    def __init__(self):
+    def __init__(self) -> None:
         super(BinaryPropagator, self).__init__()
 
-    def inject(self, span_context, carrier, disable_w3c_trace_context=True):
+    def inject(
+        self,
+        span_context: SpanContext,
+        carrier: CarrierT,
+        disable_w3c_trace_context: bool = True,
+    ) -> Optional[CarrierT]:
         try:
             trace_id = format_span_id(span_context.trace_id).encode()
             span_id = format_span_id(span_context.span_id).encode()
@@ -37,21 +44,23 @@ class BinaryPropagator(BasePropagator):
             if disable_w3c_trace_context:
                 traceparent, tracestate = [None] * 2
             else:
-                traceparent, tracestate = self._get_participating_trace_context(span_context)
+                traceparent, tracestate = self._get_participating_trace_context(
+                    span_context
+                )
                 try:
-                    traceparent = str.encode(traceparent)
-                    tracestate = str.encode(tracestate)
+                    traceparent = str.encode(traceparent)  # type: ignore[arg-type]
+                    tracestate = str.encode(tracestate)  # type: ignore[arg-type]
                 except Exception:
                     traceparent, tracestate = [None] * 2
 
             if isinstance(carrier, dict) or hasattr(carrier, "__dict__"):
                 if traceparent and tracestate:
-                    carrier[self.HEADER_KEY_TRACEPARENT] = traceparent
-                    carrier[self.HEADER_KEY_TRACESTATE] = tracestate
-                carrier[self.HEADER_KEY_T] = trace_id
-                carrier[self.HEADER_KEY_S] = span_id
-                carrier[self.HEADER_KEY_L] = level
-                carrier[self.HEADER_SERVER_TIMING] = server_timing
+                    carrier[self.HEADER_KEY_TRACEPARENT] = traceparent  # type: ignore[index]
+                    carrier[self.HEADER_KEY_TRACESTATE] = tracestate  # type: ignore[index]
+                carrier[self.HEADER_KEY_T] = trace_id  # type: ignore[index]
+                carrier[self.HEADER_KEY_S] = span_id  # type: ignore[index]
+                carrier[self.HEADER_KEY_L] = level  # type: ignore[index]
+                carrier[self.HEADER_SERVER_TIMING] = server_timing  # type: ignore[index]
             elif isinstance(carrier, list):
                 if traceparent and tracestate:
                     carrier.append((self.HEADER_KEY_TRACEPARENT, traceparent))
@@ -62,13 +71,17 @@ class BinaryPropagator(BasePropagator):
                 carrier.append((self.HEADER_SERVER_TIMING, server_timing))
             elif isinstance(carrier, tuple):
                 if traceparent and tracestate:
-                    carrier = carrier.__add__(((self.HEADER_KEY_TRACEPARENT, traceparent),))
-                    carrier = carrier.__add__(((self.HEADER_KEY_TRACESTATE, tracestate),))
+                    carrier = carrier.__add__(
+                        ((self.HEADER_KEY_TRACEPARENT, traceparent),)
+                    )
+                    carrier = carrier.__add__(
+                        ((self.HEADER_KEY_TRACESTATE, tracestate),)
+                    )
                 carrier = carrier.__add__(((self.HEADER_KEY_T, trace_id),))
                 carrier = carrier.__add__(((self.HEADER_KEY_S, span_id),))
                 carrier = carrier.__add__(((self.HEADER_KEY_L, level),))
                 carrier = carrier.__add__(((self.HEADER_SERVER_TIMING, server_timing),))
-            elif hasattr(carrier, '__setitem__'):
+            elif hasattr(carrier, "__setitem__"):
                 if traceparent and tracestate:
                     carrier.__setitem__(self.HEADER_KEY_TRACEPARENT, traceparent)
                     carrier.__setitem__(self.HEADER_KEY_TRACESTATE, tracestate)
@@ -82,5 +95,3 @@ class BinaryPropagator(BasePropagator):
             return carrier
         except Exception:
             logger.debug("inject error:", exc_info=True)
-
-

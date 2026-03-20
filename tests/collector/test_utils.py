2b3545e5f5eaea84eb9dfd4dd69bb90d6dee0f42
Paulo Vital <paulo.vital@ibm.com>
2026-03-20 09:40:34 +0100

    refactor(tests): Use context parameter instead of span_context
    This commit updates all tests to use the `context` parameter instead of
    the deprecated `span_context` parameter when calling `start_span()` and
    `start_as_current_span()` methods.
    
    This commit fixes #847.
    
    Signed-off-by: Paulo Vital <paulo.vital@ibm.com>
    Co-authored-by: Varsha GS <varsha.gs@ibm.com>

--- tests/collector/test_utils.py
+++ tests/collector/test_utils.py
@@ -1,15 +1,16 @@
 # (c) Copyright IBM Corp. 2025
 
 
-import pytest
 from typing import Generator
+
+import pytest
+from opentelemetry.context.context import Context
+from opentelemetry.trace.span import format_span_id
+
 from instana.collector.utils import format_span
 from instana.singletons import get_tracer
 from instana.span.registered_span import RegisteredSpan
 from instana.span.span import get_current_span
-from opentelemetry.trace.span import format_span_id
-
-from instana.span_context import SpanContext
 
 
 class TestUtils:
@@ -17,13 +18,13 @@ class TestUtils:
     def _resource(self) -> Generator[None, None, None]:
         self.tracer = get_tracer()
         self.recorder = self.tracer.span_processor
-        self.span_context = None
+        self.context = None
         yield
 
-    def test_format_span(self, span_context: SpanContext) -> None:
-        self.span_context = span_context
+    def test_format_span(self, context: Context) -> None:
+        self.context = context
         with self.tracer.start_as_current_span(
-            name="span1", span_context=self.span_context
+            name="span1", context=self.context
         ) as pspan:
             expected_trace_id = format_span_id(pspan.context.trace_id)
             expected_span_id = format_span_id(pspan.context.span_id)
@@ -46,4 +47,5 @@ class TestUtils:
         assert formatted_spans[1].p == formatted_spans[0].s
         assert formatted_spans[1].k == 1
         assert formatted_spans[1].s != formatted_spans[0].s
+        assert formatted_spans[1].n == "span2"
         assert formatted_spans[1].n == "span2"

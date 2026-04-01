b44de6e05314c39a68a0b61be0b70f324246e571
Varsha GS <varsha.gs@ibm.com>
2026-04-01 18:00:07 +0530

    refactor(tests): Adapt `SDK` spans to use the `kind` parameter
    Signed-off-by: Varsha GS <varsha.gs@ibm.com>

--- tests/span/test_span_sdk.py
+++ tests/span/test_span_sdk.py
@@ -1,8 +1,8 @@
 # (c) Copyright IBM Corp. 2024
 
 from typing import Generator, Tuple
-
 import pytest
+from opentelemetry.trace import SpanKind
 
 from instana.recorder import StanRecorder
 from instana.span.sdk_span import SDKSpan
@@ -24,12 +24,11 @@ class TestSDKSpan:
         span_name = "test-sdk-span"
         service_name = "test-sdk"
         attributes = {
-            "span.kind": "entry",
             "arguments": "--quiet",
             "return": "True",
         }
         self.span = InstanaSpan(
-            span_name, span_context, span_processor, attributes=attributes
+            span_name, span_context, span_processor, attributes=attributes, kind=SpanKind.SERVER
         )
         sdk_span = SDKSpan(self.span, None, service_name)
 
@@ -40,9 +39,9 @@ class TestSDKSpan:
                 "service": service_name,
                 "sdk": {
                     "name": span_name,
-                    "type": attributes["span.kind"],
+                    "type": "entry",
                     "custom": {
-                        "attributes": attributes,
+                        "tags": attributes,
                     },
                     "arguments": attributes["arguments"],
                     "return": attributes["return"],
@@ -66,12 +65,15 @@ class TestSDKSpan:
         "span_kind, expected_result",
         [
             (None, ("intermediate", 3)),
+            (SpanKind.INTERNAL, ("intermediate", 3)),
             ("entry", ("entry", 1)),
             ("server", ("entry", 1)),
             ("consumer", ("entry", 1)),
+            (SpanKind.SERVER, ("entry", 1)),
             ("exit", ("exit", 2)),
             ("client", ("exit", 2)),
             ("producer", ("exit", 2)),
+            (SpanKind.CLIENT, ("exit", 2)),
         ],
     )
     def test_sdkspan_get_span_kind(
@@ -81,11 +83,8 @@ class TestSDKSpan:
         span_kind: str,
         expected_result: Tuple[str, int],
     ) -> None:
-        attributes = {
-            "span.kind": span_kind,
-        }
         self.span = InstanaSpan(
-            "test-sdk-span", span_context, span_processor, attributes=attributes
+            "test-sdk-span", span_context, span_processor, kind=span_kind
         )
         sdk_span = SDKSpan(self.span, None, "test")
 
@@ -93,7 +92,7 @@ class TestSDKSpan:
 
         assert expected_result == kind
 
-    def test_sdkspan_get_span_kind_with_no_attributes(
+    def test_sdkspan_get_span_kind_default(
         self,
         span: InstanaSpan,
     ) -> None:

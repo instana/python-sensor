16a95494bbf9dc8e56366ad548dc2a5ff0abb184
Paulo Vital <paulo.vital@ibm.com>
2026-05-15 14:11:56 +0200

    chore(version): Bump version to 3.15.0.
    Signed-off-by: Paulo Vital <paulo.vital@ibm.com>

--- src/instana/version.py
+++ src/instana/version.py
@@ -1,6 +1,6 @@
-# (c) Copyright IBM Corp. 2021
+# (c) Copyright IBM Corp. 2021, 2026
 # (c) Copyright Instana Inc. 2020
 
 # Module version file.  Used by setup.py and snapshot reporting.
 
-VERSION = "3.14.0"
+VERSION = "3.15.0"
